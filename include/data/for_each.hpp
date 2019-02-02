14fcaf69f066050104c138fa96c75b9f874600e7
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2019-02-01 19:48:18 -0600

    Update for_each to apply to queues

--- include/data/for_each.hpp
+++ include/data/for_each.hpp
@@ -2,11 +2,13 @@
 #define DATA_FOR_EACH_HPP
 
 #include <data/list/for_each.hpp>
+#include <data/queue/for_each.hpp>
 #include <data/map.hpp>
 #include <data/meta/which.hpp>
 #include <data/meta/equal.hpp>
 #include <data/list/linked_list.hpp>
 #include <data/map/rb.hpp>
+#include <data/queue/functional_queue.hpp>
 #include <data/tools/entry_function.hpp>
 
 namespace data {
@@ -24,6 +26,17 @@ namespace data {
             
             output operator()(const function f, const from l) const {
                 return list::for_each<function, from, output>{}(f, l);
+            }
+        };
+        
+        template <typename function, typename from> 
+        struct for_each_queue {
+            using input_element = typename list::is_list<from>::element;
+            using output_element = typename std::__invoke_result<function, input_element>::type;
+            using output = functional_queue<linked_list<output_element>>;
+            
+            output operator()(const function f, const from l) const {
+                return queue::for_each<function, from, output>{}(f, l);
             }
         };
         
@@ -49,19 +62,19 @@ namespace data {
             using key = typename requirement::key;
             using value = typename requirement::value;
             using list = typename requirement::list;
-            using list_function = typename Which<for_each_list<entry_function<key, function, value>, list>, for_each_list<value_function<key, function, value>, list>>::result;
-            using inner_function = decltype(list_function::Function);
+            using outer_function = typename Which<for_each_list<entry_function<key, function, value>, list>, for_each_list<value_function<key, function, value>, list>>::result;
+            using inner_function = decltype(outer_function::Function);
             using output_value = typename inner_function::output_element::value;
             using output = rb_map<key, output_value>;
             
             output operator()(const function f, const from m) const {
-                return {list_function{}(inner_function{f}, map::entries(m))};
+                return {outer_function{}(inner_function{f}, map::entries(m))};
             }
         };
         
         template <typename function, typename from> 
         struct for_each {
-            using inner_function = typename Which<for_each_list<function, from>, for_each_map<function, from>>::result;
+            using inner_function = typename Which<for_each_queue<function, from>, for_each_list<function, from>, for_each_map<function, from>>::result;
             using output = typename inner_function::output;
             
             output operator()(const function f, const from m) const {
@@ -85,6 +98,18 @@ namespace data {
             public meta::Equal<typename list::is_list<List>::element, A> {
             
             linked_list<B> use_case(f fun, List a) {
+                return data::for_each(fun, a);
+            }
+            
+        };
+        
+        template <typename A, typename B, typename f, typename Queue>
+        struct for_each_queue :
+            public data:: function_definition<f, A, B>,
+            public queue::is_queue<Queue>, 
+            public meta::Equal<typename queue::is_queue<Queue>::element, A> {
+            
+            functional_queue<linked_list<B>> use_case(f fun, Queue a) {
                 return data::for_each(fun, a);
             }
             
