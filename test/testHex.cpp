829ba2c479588ec0894fac78f56bc77a381aef66
Katrina Swales <kat.swales@nekokittygames.com>
2019-07-05 01:21:59 +0100

    moved tests over to Unit Testing framework Google Tests

---
+++ test/testHex.cpp
@@ -0,0 +1,35 @@
+//
+// Created by nekosune on 05/07/19.
+//
+
+#include "data/encoding/hex.hpp"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "gmock/gmock-matchers.h"
+namespace {
+
+TEST(HexTest, HexToArray) {
+    const char* inputHex="0063EA172D63808C";
+    data::encoding::hex::string hexString(std::string("0063EA172D63808C"));
+    ASSERT_THAT(static_cast<data::bytes>(hexString),::testing::ElementsAre(0,99,234,23,45,99,128,140));
+}
+
+TEST(HexTest, ValidHexString) {
+    ASSERT_TRUE(data::encoding::hex::valid("012345abcd"));
+    ASSERT_TRUE(data::encoding::hex::valid("012345ABCD"));
+    ASSERT_TRUE(data::encoding::hex::valid("012345aBcD"));
+    ASSERT_FALSE(data::encoding::hex::valid("012345abc"));
+    ASSERT_FALSE(data::encoding::hex::valid("012345aHcd"));
+}
+
+TEST(HexTest,InvalidExceptionOnError) {
+    ASSERT_THROW(data::encoding::hex::string malformedHexString(std::string("0063EA172D63808")),data::encoding::hex::invalid );
+}
+
+TEST(HexTest,WriteBytes) {
+        data::bytes testArray{0,99,234,23,45,99,128,140};
+        ASSERT_STREQ(data::encoding::hex::write(testArray).c_str(),"0063EA172D63808C");
+}
+
+
+}
--- test/testLib.cpp
+++ test/testLib.cpp
@@ -1,66 +1,11 @@
 //
 // Created by nekosune on 04/07/19.
 //
-#include <data/crypto/sha256.hpp>
-#include <data/encoding/hex.hpp>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <c++/6/cstring>
 
-std::vector<uint8_t> convertString(const char* temp) {
-    std::vector<uint8_t> ret;
-    if(strlen(temp)==0)
-        return ret;
-    int i=0;
-    uint8_t chr=temp[i];
-
-    while (chr!=0)
-    {
-        ret.push_back(temp[i]);
-        i++;
-        chr=temp[i];
-    }
-}
+#include <gtest/gtest.h>
 
 int main(int argc, char *argv[]){
-    data::sha256::digest temp=data::sha256::hash(convertString("I am a Poppy"));
 
-    std::stringstream ss;
-    for(int i=0; i<temp.size(); ++i)
-        ss << std::hex << (int)temp.data()[i];
-    std::string mystr = ss.str();
-    std::cout << "Test String is: " << "I am a Poppy" << std::endl;
-    std::cout << "SHA256 of test string is : " << mystr.c_str() << std::endl;
-    std::cout << "Test array is: " << "0,99,234,23,45,99,128,140" << std::endl;
-    std::cout << "Test array as hex string: " << "0063EA172D63808C" << std::endl;
-    data::encoding::hex::string hexString(std::string("0063EA172D63808C"));
-
-    std::stringstream ss2;
-    for(int i=0;i< static_cast<data::bytes>(hexString).size();i++)
-    {
-        ss2 << std::dec << (int)static_cast<data::bytes>(hexString)[i] << ", ";
-    }
-    std::string myArray = ss2.str();
-    std::cout << "Hex output is : " << myArray.c_str() << std::endl;
-    std::cout << std::endl << "Test Exception handling " << std::endl << std::endl;
-    std::cout << "Test array as malformed hex string: " << "0063EA172D63808" << std::endl;
-    try {
-        data::encoding::hex::string malformedHexString(std::string("0063EA172D63808"));
-    }
-    catch (data::encoding::hex::invalid inv) {
-        std::cout << "Exception: " <<  inv.what()<< std::endl;
-    }
-
-    std::cout << std::endl << "Checking valid function" << std::endl << std::endl;
-    std::cout << "Valid string (lowercase) : " << data::encoding::hex::valid("012345abcd") << std::endl;
-    std::cout << "Valid string (uppercase) : " << data::encoding::hex::valid("012345ABCD") << std::endl;
-    std::cout << "Valid string (mixed) : " << data::encoding::hex::valid("012345aBcD") << std::endl;
-    std::cout << "String too short: " << data::encoding::hex::valid("012345abc") << std::endl;
-    std::cout << "Invalid character: " << data::encoding::hex::valid("012345aHcd") << std::endl;
-    std::cout << std::endl << "Test write function" << std::endl << std::endl;
-    data::bytes testArray{0,99,234,23,45,99,128,140};
-    std::cout << "Write function outputs: " << data::encoding::hex::write(testArray) << std::endl;
-
-
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
