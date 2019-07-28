50316b5f7e4a80cd4c35c14ec5ba48696b984b02
Katrina Swales <kat.swales@nekokittygames.com>
2019-07-28 16:36:23 +0100

    Fixed some missing type names

--- include/data/types.hpp
+++ include/data/types.hpp
@@ -16,9 +16,11 @@ namespace data {
     using array = const std::array<X, n>;
 
     using byte = uint8_t;
-    using uint16 = uint16;
+    using uint16 = uint16_t;
     using uint32 = uint32_t;
     using uint64 = uint64_t;
+    using int8 = int8_t ;
+    using int16 = int16_t;
     using int32 = int32_t;
     using int64 = int64_t;
     
