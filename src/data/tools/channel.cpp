501fcb6390d1e7860576045844f2587607178c7e
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2019-09-05 19:27:24 -0500

    Add implementations of channel member functions.

---
+++ src/data/tools/channel.cpp
@@ -0,0 +1,44 @@
+#include <data/tools/channel.hpp>
+
+namespace data {
+
+    template <class item> 
+    void channel<item>::inner::close() {
+        std::unique_lock<std::mutex> lock(M);
+        Closed = true;
+        Receive.notify_all();
+    }
+
+    template <class item> 
+    bool channel<item>::inner::closed() const {
+        std::unique_lock<std::mutex> lock(M);
+        return closed;
+    }
+
+    template <class item> 
+    void channel<item>::inner::put(const item &i) {
+        std::unique_lock<std::mutex> lock(M);
+        if(Closed) throw std::logic_error("put to closed channel");
+        Queue.push_back(i);
+        Receive.notify_one();
+        if (Size == 0 || Queue.size() < Size) return;
+        else Send.wait(lock);
+    }
+        
+    template <class item> 
+    bool channel<item>::inner::get(item &out, bool wait) {
+        std::unique_lock<std::mutex> lock(M);
+        if (Queue.empty()) {
+            if (!wait || Closed) return false;
+            else {
+                Receive.wait(lock);
+                if (Closed) return false;
+            }
+        } 
+        out = Queue.front();
+        Queue.pop_front();
+        Send.notify_one();
+        return true;
+    }
+
+}
