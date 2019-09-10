b806e793143bb8cc3bf7249b3e1353f6195df412
Katrina Swales <kat.swales@nekokittygames.com>
2019-09-10 11:32:55 +0100

    Fixed sha256 and added needed function

--- include/data/crypto/sha256.hpp
+++ include/data/crypto/sha256.hpp
@@ -6,6 +6,7 @@
 #define DATA_CRYPTO_SHA256
 
 #include <data/crypto/digest.hpp>
+#include <crypto++/sha.h>
 
 namespace data::sha256 {
     
@@ -16,9 +17,15 @@ namespace data::sha256 {
     const digest Zero = digest{};
     
     digest hash(bytes&);
-    
+    digest hash(slice<byte>);
+
     template <uint32 n>
-    digest hash(const std::array<byte, n>&);
+    digest hash(const std::array<byte, n>& data){
+        std::array<byte, size> hash;
+        CryptoPP::SHA256 shaHash;
+        shaHash.CalculateDigest(hash.data(), data.data(), data.size());
+        return uint<size>{hash};
+    };
     
     template <uint32 n>
     digest hash(const uint<n>&);
