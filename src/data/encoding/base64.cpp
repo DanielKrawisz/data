c90b30f7a2391b60a259c4ce6bd9ff4886c38804
Katrina Swales <kat.swales@nekokittygames.com>
2019-10-03 17:09:50 +0100

    Implemented base64

--- src/data/encoding/base64.cpp
+++ src/data/encoding/base64.cpp
@@ -1,35 +1,30 @@
 // Copyright (c) 2019 Katrina Swales
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#include <data/encoding/base32.hpp>
-#include <bitcoin/system/formats/base_58.hpp>
+#include <data/encoding/base64.hpp>
 #include <data/encoding/invalid.hpp>
-#include <cryptopp/base32.h>
-namespace data::encoding::base32 {
+#include <cryptopp/base64.h>
+#include <bitcoin/system/formats/base_58.hpp>
+#include <bitcoin/system/formats/base_64.hpp>
+
+namespace data::encoding::base64 {
 
     bytes read(const std::string & s) {
-        std::vector<byte> out;
-        CryptoPP::Base32Decoder decoder;
-        decoder.Put((byte*)s.data(),s.size());
-        decoder.MessageEnd();
-        CryptoPP::word64 size = decoder.MaxRetrievable();
-        if(size && size <= SIZE_MAX)
-        {
-            out.resize(size);
-            decoder.Get((byte*)&out[0],out.size());
-        }
+        libbitcoin::system::data_chunk out;
+        if (!libbitcoin::system::decode_base64(out, s)) return {};
         return out;
     }
 
     string::string(std::string str) {
         auto *tmp=new std::vector<uint8_t>() ;
-        if(!libbitcoin::system::decode_base58(*tmp,str))
+        if(!libbitcoin::system::decode_base64(*tmp,str))
             throw invalid(format, *this);
         ToBytes=tmp;
     }
 
     bool valid(const std::string &str) {
-        return libbitcoin::system::is_base58(str);
+        libbitcoin::system::data_chunk out;
+        return libbitcoin::system::decode_base58(out,str);
     }
 
 
---
+++ test/testBase64.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2019 Katrina Swales
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "data/encoding/base64.hpp"
+#include "data/encoding/invalid.hpp"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "gmock/gmock-matchers.h"
+namespace {
+
+
+
+    TEST(Base64Test, Base64HexToArray) {
+        data::encoding::base64::string base64String(std::string("KzFvxm6N9qW11MbVoZM8c3tp6UHqf1qrh9EMcHPj74cgBWRmRvBS"));
+        data::bytes bytes=static_cast<data::bytes>(base64String);
+        EXPECT_THAT(bytes,::testing::ElementsAre(0x80,0x5A,0xA7,0x86,0xA5,0x7B,0x3B,0xFC,0x0D,0xFD,0xF2,0xEC,0x86,0x76,0x03,0x39,0xF0,0x18,0x11,0x4A,0x7E,0x30,0xC2,0xD2,0x70,0x1C,0xF2,0x94,0xDC,0x60,0x82,0x9D,0x9B,0x01,0x1C,0xD8,0xE3,0x91));
+    }
+
+    TEST(Base64Test, Base64ValidHexString) {
+        EXPECT_TRUE(data::encoding::base64::valid("KzFvxm6N9qW11MbVoZM8c3tp6UHqf1qrh9EMcHPj74cgBWRmRvBS"));
+        EXPECT_TRUE(data::encoding::base64::valid("3m8npvpNDU6k8zcAH8RBcUZeDLWx"));
+        EXPECT_FALSE(data::encoding::base64::valid("KzFvxm6N9qW11MbVoZM8c3tp6UHqf1qrh9EMIHPj74cgBWRmRvBS"));
+    }
+
+    TEST(Base64Test,Base64InvalidExceptionOnError) {
+        EXPECT_THROW(data::encoding::base64::string malformedHexString(std::string("KzFvxm6N9qW11MbVoZM8c3tp6UHqf1qrh9EMIHPj74cgBWRmRvBS")),data::encoding::invalid );
+    }
+
+    TEST(Base64Test,Base64WriteBytes) {
+        data::bytes testArray{0x80,0x5A,0xA7,0x86,0xA5,0x7B,0x3B,0xFC,0x0D,0xFD,0xF2,0xEC,0x86,0x76,0x03,0x39,0xF0,0x18,0x11,0x4A,0x7E,0x30,0xC2,0xD2,0x70,0x1C,0xF2,0x94,0xDC,0x60,0x82,0x9D,0x9B,0x01,0x1C,0xD8,0xE3,0x91};
+        ASSERT_STREQ(data::encoding::base64::write(testArray).c_str(),"KzFvxm6N9qW11MbVoZM8c3tp6UHqf1qrh9EMcHPj74cgBWRmRvBS");
+    }
+
+
+}
