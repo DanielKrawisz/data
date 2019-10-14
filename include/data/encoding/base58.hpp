e058af904c3aa6fce8d6f7ccebd127463c0d5d5f
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2019-10-13 19:42:57 -0500

    more work on base 58. I can't get it to work!

--- include/data/encoding/base58.hpp
+++ include/data/encoding/base58.hpp
@@ -15,47 +15,65 @@
 
 namespace data::encoding::base58 {
     
-    static const std::string format{"base58"};
+    const std::string format{"base58"};
     
-    static const std::string characters{"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"};
+    const std::string characters{"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"};
     
-    static constexpr auto pattern = ctll::fixed_string{"[2-9A-HJ-NP-Za-km-z][1-9A-HJ-NP-Za-km-z]*"};
+    constexpr auto pattern = ctll::fixed_string{"[2-9A-HJ-NP-Za-km-z][1-9A-HJ-NP-Za-km-z]*"};
     
-    static bool valid(const string_view s) {
+    inline bool valid(const string_view s) {
         return ctre::match<pattern>(s);
     }
     
-    static char digit(char c) {
+    inline char digit(char c) {
         return c < '1' ? -1 : c <= '9' ?  c - '1' : c < 'A' ? -1 : c <= 'H' ? c - 'A' + 9 : c < 'J' ? -1 : c <= 'N' ? c - 'J' + 17 : c < 'P' ? -1 : c <= 'Z' ? c - 'P' + 22 : c < 'a' ? -1 : c <= 'k' ? c - 'a' + 33 : c < 'm' ? -1 : c <= 'z' ? c - 'm' + 44 : -1;
     };
     
+    // cache of powers of 58.
     template <typename N>
-    static functional_queue<N, list::linked<N>>& powers() {
+    class power {
+        functional_queue<N, list::linked<N>>& Powers;
+        N First;
+        functional_queue<N, list::linked<N>> Rest;
+    public:
+        power(
+        functional_queue<N, list::linked<N>>& p, N n, 
+        functional_queue<N, list::linked<N>> r) : Powers{p}, First{n}, Rest{r} {}
+        
+        N first() const {
+            return First;
+        }
+        
+        power rest() const {
+            if (Rest.empty()) {
+                N next{First * 58};
+                Powers = Powers.append(next);
+                return power{Powers, next, Rest};
+            }
+            return power{Powers, Rest.first(), Rest.rest()};
+        }
+    };
+    
+    // note: not good for multithreading. 
+    template <typename N>
+    power<N> powers() {
         static functional_queue<N, list::linked<N>> Powers = functional_queue<N, list::linked<N>>::make(N{1});
-        return Powers;
+        return power<N>{Powers, Powers.first(), Powers.rest()};
     }
     
     template <typename N>
-    static N read(const string_view s) {
+    N read(const string_view s) {
         if (s.size() == 0) return N{};
         
-        while (powers<N>().size() < s.size()) {
-            powers<N>() = powers<N>().append(powers<N>().last() * 58);
-        }
-        
-        functional_queue<N, list::linked<N>> Powers = powers<N>();
+        N power{1};
         
         N n{0};
         
-        auto i = s.end();
-        while(i != s.begin()) {
-            i--;
-            char v = digit(*i);
+        for (char x : s) {
+            char v = digit(x);
             if (v == -1) return N{0};
-            
-            n = n + (Powers.first() * uint64(digit));
-        
-            Powers = Powers.rest();
+            n += power * uint64(v);
+            power *= 58;
         }
         
         return n;
@@ -64,16 +82,13 @@ namespace data::encoding::base58 {
     template <typename N>
     string write(N n) {
         if (n == 0) return "1";
-        functional_queue<N, list::linked<N>> Powers = powers<N>();
-        while(powers<N>().last() < n) {
-            powers<N>() = powers<N>().append(powers<N>().last() * 58);
-        }
+        
+        N power{1};
         
         uint32 size = 0;
-        Powers = powers<N>();
-        while(Powers.first() <= n) {
+        while(power <= n) {
             size ++;
-            Powers = Powers.rest();
+            power *= 58;
         }
         
         string s{static_cast<char>(size)};
--- test/testLinkedList.cpp
+++ test/testLinkedList.cpp
@@ -20,7 +20,7 @@ TEST(LinkedListTest, TestLinkedList1) {
     
 }
 
-TEST(LinkedListTest, DISABLED_TestLinkedList2) {
+TEST(LinkedListTest, TestLinkedList2) {
     using namespace data::exported;
     
     auto L1 = list<int>::make(1, 2, 3);
@@ -28,6 +28,11 @@ TEST(LinkedListTest, DISABLED_TestLinkedList2) {
     
     EXPECT_FALSE(L1 == L2);
     EXPECT_TRUE(L1 == reverse(L2));
+    
+}
+
+TEST(LinkedListTest, DISABLED_TestLinkedListSort) {
+    using namespace data::exported;
     
     EXPECT_TRUE(list<int>::make(1, 2, 3, 4, 5) == data::sort(list<int>::make(4, 5, 1, 3, 2)));
     EXPECT_TRUE(list<int>::make(1, 2, 3, 3, 4, 5) == data::sort(list<int>::make(4, 3, 5, 1, 3, 2)));
