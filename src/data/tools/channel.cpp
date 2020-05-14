850b635c5c4e7d73e063c60fd94444b9b49b7e2e
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2020-05-14 03:28:47 -0500

    Change () to {} in constructors in channel.cpp

--- src/data/tools/channel.cpp
+++ src/data/tools/channel.cpp
@@ -4,20 +4,20 @@ namespace data::tool {
 
     template <class item> 
     void channel<item>::inner::close() {
-        std::unique_lock<std::mutex> lock(M);
+        std::unique_lock<std::mutex> lock{M};
         Closed = true;
         Receive.notify_all();
     }
 
     template <class item> 
     bool channel<item>::inner::closed() const {
-        std::unique_lock<std::mutex> lock(M);
+        std::unique_lock<std::mutex> lock{M};
         return closed;
     }
 
     template <class item> 
     void channel<item>::inner::put(const item &i) {
-        std::unique_lock<std::mutex> lock(M);
+        std::unique_lock<std::mutex> lock{M};
         if(Closed) throw std::logic_error("put to closed channel");
         Queue.push_back(i);
         Receive.notify_one();
@@ -27,7 +27,7 @@ namespace data::tool {
         
     template <class item> 
     bool channel<item>::inner::get(item &out, bool wait) {
-        std::unique_lock<std::mutex> lock(M);
+        std::unique_lock<std::mutex> lock{M};
         if (Queue.empty()) {
             if (!wait || Closed) return false;
             else {
