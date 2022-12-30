dfd37022e2774fe1789c140c54e4c03033c5e7a1
Daniel Krawisz <danielkrawisz@protonmail.com>
2022-12-30 16:55:22 -0500

    fix up some things, get started on external program session

--- src/data/networking/JSON.cpp
+++ src/data/networking/JSON.cpp
@@ -1,15 +1,17 @@
 #include <data/networking/JSON.hpp>
 
-void data::networking::JSON_line_connection::write(const byte *data, size_t size) {
+void data::networking::JSON_line_connection::write(const char *data, size_t size) {
     int last_new_line = 0;
-    for (int i = 0; i < size; i++) if ((const char)(data[i]) == '\n') {
-        Stream.write((const char*)(data + last_new_line), i);
+    for (int i = 0; i < size; i++) if (data[i] == '\n') {
+        Stream.write(data + last_new_line, i);
+
         try {
             parsed(JSON::parse(Stream.str()));
         } catch (const JSON::exception &x) {
             parse_error(Stream.str());
         }
-        Stream = std::stringstream{};
+
+        Stream = std::stringstream {};
         last_new_line = i + 1;
     }
     
