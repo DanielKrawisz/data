871688e8732deb62d64379d8775e0eab297779f9
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-01-04 19:51:41 -0500

    I don't know

--- include/data/io/run.hpp
+++ include/data/io/run.hpp
@@ -4,20 +4,23 @@
 #ifndef DATA_IO_RUN
 #define DATA_IO_RUN
 
-#include <data/networking/asio/message_queue.hpp>
+#include <data/networking/asio/async_to.hpp>
+#include <data/networking/asio/async_from.hpp>
 #include <boost/process.hpp>
 
 namespace data::io {
 
-    struct session : networking::asio::message_queue<session, boost::process::async_pipe, string_view, const string &> {
+    struct session : networking::asio::session<string_view, const string &>,
+        networking::asio::async_to<boost::process::async_pipe, char>,
+        networking::asio::async_from<boost::process::async_pipe, char> {
         virtual ~session() {}
 
-        virtual int close() = 0;
-        virtual void error (string_view) = 0;
+        void close() final override;
+        void handle_error (const io_error &err) final override;
     };
     
     // run an external command with standard in and standard out connected. 
-    ptr<session> run(io::io_context &, string command, std::function<bool (string_view)> out, std::function<bool (string_view)> err);
+    ptr<session> run(boost::asio::io_context &, string command, std::function<bool (string_view)> out, std::function<bool (string_view)> err);
     
 }
 
