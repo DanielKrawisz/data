70a0238c5bb7a84e97927b0c03ee633137ceea38
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-01-06 15:46:44 -0500

    can compile external programs and TCP sessions

--- include/data/io/run.hpp
+++ include/data/io/run.hpp
@@ -4,23 +4,55 @@
 #ifndef DATA_IO_RUN
 #define DATA_IO_RUN
 
-#include <data/networking/asio/session.hpp>
+#include <data/networking/asio/async_message_queue.hpp>
+#include <boost/process.hpp>
 
 namespace data::io {
 
-    struct process {
-        void send (const string &);
+    struct process;
 
-        int exit ();
-        bool closed ();
-        void handle_error (const io_error &err);
+    using output_handler = networking::receive_handler<process, string_view>;
+
+    using error = networking::asio::io_error;
+
+    ptr<process> run (boost::asio::io_context &, string command,
+        output_handler std_out_handler,
+        output_handler std_err_handler,
+        function<void (error)> err_handler);
+
+    using pipe = boost::process::async_pipe;
+
+    struct process {
+
+        boost::process::child Child;
+
+        networking::asio::async_message_queue<pipe, char> Queue;
+
+        void send (const string &x) {
+            Queue.write(x);
+        }
+
+        int close () {
+            Child.terminate ();
+            return Child.exit_code ();
+        }
+
+        bool closed () {
+            return !Child.running ();
+        }
+
+        ~process () {
+            close ();
+        }
+
+    private:
+        process (boost::process::child &&child, networking::asio::async_message_queue<pipe, char> q) : Child{std::move (child)}, Queue {q} {}
+
+        friend ptr<process> run (boost::asio::io_context &, string command,
+            output_handler std_out_handler,
+            output_handler std_err_handler,
+            function<void (error)> err_handler);
     };
-    
-    // run an external command with standard in and standard out connected. 
-    ptr<session> run (boost::asio::io_context &, string command,
-        networking::receive_handler<process, string_view> std_out_handler,
-        networking::receive_handler<process, string_view> std_err_handler,
-        std::function<bool (io_error)> err_handler);
     
 }
 
--- src/data/io/run.cpp
+++ src/data/io/run.cpp
@@ -1,66 +1,31 @@
 // Copyright (c) 2022 Daniel Krawisz
 // Distributed under the Open BSV software license, see the accompanying file LICENSE.
 
-#include <boost/process.hpp>
 #include <data/io/run.hpp>
-#include <data/networking/asio/async_message_queue.hpp>
 #include <data/networking/asio/async_wait_for_message.hpp>
 
 namespace data::io {
+
     namespace bp = boost::process;
 
-    struct program_session final : session, std::enable_shared_from_this<program_session> {
-
-        bp::async_pipe In;
-        bp::async_pipe Out;
-        bp::async_pipe Err;
-
-        bp::child Child;
-
-        ~program_session () {
-            close ();
-        }
-
-        std::function<void (string_view)> Receive;
-        std::function<void (string_view)> Error;
-
-        void receive (string_view x) final override {
-            if (!Receive (x)) Child.terminate ();
-        }
-
-        void error (string_view x) final override {
-            if (!Error (x)) Child.terminate ();
-        }
-
-        program_session (boost::asio::io_context &i, string command, std::function<void (string_view)> out, std::function<void (string_view)> err):
-            In {i}, Out {i}, Err {i}, Child{command, bp::std_out > Out, bp::std_err > Err, bp::std_in < In} {
-
-            io::async_read(Out, boost::asio::buffer(buf), [](const boost::system::error_code &ec, std::size_t size) {
-                throw 0;
-            });
-
-            io::async_read(Err, boost::asio::buffer(buf), [](const boost::system::error_code &ec, std::size_t size) {
-                throw 0;
-            });
-        }
-
-        int close () final override {
-            Child.terminate ();
-            return Child.exit_code ();
-        }
-
-        bool closed () final override {
-            return !chlid.running ();
-        }
-
-        void send (string_view x) final override {
-            x >> In;
-        }
-    };
-
     // run an external command with standard in and standard out connected.
-    ptr<session> run (io::io_context &cc, string command, std::function<void (string_view)> out, std::function<void (string_view)> err) {
-        return std::static_pointer_cast<session> (ptr<program_session> (new program_session {cc, command, out, err}));
+    ptr<process> run (boost::asio::io_context &io, string command,
+        output_handler std_out_handler,
+        output_handler std_err_handler,
+        function<void (error)> err_handler) {
+
+        ptr<bp::async_pipe> in { new bp::async_pipe {io}};
+        ptr<bp::async_pipe> out { new bp::async_pipe {io}};
+        ptr<bp::async_pipe> err { new bp::async_pipe {io}};
+
+        bp::child child{command, bp::std_out > *out, bp::std_err > *err, bp::std_in < *in};
+
+        ptr<process> p {new process {std::move (child), networking::asio::async_message_queue<pipe, char> {in, err_handler}}};
+
+        networking::asio::async_wait_for_message<pipe, char> (out, std_out_handler (p), err_handler);
+        networking::asio::async_wait_for_message<pipe, char> (err, std_err_handler (p), err_handler);
+
+        return p;
     }
 
 }
