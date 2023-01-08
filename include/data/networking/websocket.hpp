f818c38d5e3b59a7b389ca0e5c6dea7107402c36
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-01-08 14:37:25 -0500

    Websocket hpp?

--- include/data/networking/websocket.hpp
+++ include/data/networking/websocket.hpp
@@ -4,15 +4,53 @@
 #ifndef DATA_NETWORKING_WEBSOCKET
 #define DATA_NETWORKING_WEBSOCKET
 
-#include <data/networking/session.hpp>
-#include <boost/asio.hpp>
+#include <data/networking/asio/async_stream_session.hpp>
 
 namespace data::networking::websocket {
     namespace io = boost::asio;
+
+    struct open {
+        io::io_context &IO;
+        string URL;
+
+        ptr<session<const string &>> operator() (receive_session<session<const string &>, string_view>);
+    };
     
-    static ptr<session<bytes_view>> connect(io::io_context &, string url, std::function<bool (bytes_view)>);
-    
-    // Notes: destructor must close session. 
+    struct insecure : asio::async_stream_session<session<const string &>, websocket::stream<beast::tcp_stream>, char> {
+        using asio::async_stream_session<session<const string &>, websocket::stream<beast::tcp_stream>, char>::async_stream_session;
+
+        void close () final override {
+            throw method::unimplemented {"websocket::insecure::close"};
+        }
+
+        bool closed () final override {
+            throw method::unimplemented {"websocket::insecure::closed"};
+        }
+
+        function<void (io_error)> HandleError;
+
+        ~insecure () {
+            close ();
+        }
+    };
+
+    struct secure : async_stream_session<session<const string &>, websocket::stream<beast::ssl_stream<beast::tcp_stream>>, char> {
+        using asio::async_stream_session<session<const string &>, websocket::stream<beast::ssl_stream<beast::tcp_stream>>, char>::async_stream_session;
+
+        void close () final override {
+            throw method::unimplemented {"websocket::secure::close"};
+        }
+
+        bool closed () final override {
+            throw method::unimplemented {"websocket::secure::closed"};
+        }
+
+        function<void (io_error)> HandleError;
+
+        ~secure () {
+            close ();
+        }
+    };
 }
 
 #endif
