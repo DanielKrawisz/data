6b0c3e22931a32dc5556fd5a07297df91bae1481
KatrinaAS <kat@katcodes.dev>
2023-01-13 13:33:54 +0000

    feat: Add initial base skeleton for new HTTP Added the base skeleton for the new http stuff. For now kept in an experimental namespace until removal of old.

---
+++ src/data/networking/nHTTP.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Katrina Knight
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <data/networking/nHTTP.hpp>
+
+namespace data::networking::experimental {
+
+}
