f1b50e838b7fafff7576e2de86d8e88b475d2bfb
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-01-16 10:39:42 -0500

    New port type

--- src/data/networking/REST.cpp
+++ src/data/networking/REST.cpp
@@ -7,23 +7,23 @@
 namespace data::networking {
     namespace {
         
-        string encode_form_data(map<string, string> form_data) {
+        string encode_form_data (map<string, string> form_data) {
             string newBody;
             
-            if(form_data.size()>0) {
+            if (form_data.size ()>0) {
                 for (const auto &it : form_data) {
                     newBody.append(it.Key+"="+it.Value+"&");
                 }
-                newBody.pop_back();
+                newBody.pop_back ();
             }
             
             return newBody;
         }
     }
 
-    HTTP::request REST::POST(string path, map<string, string> params) const {
-        return HTTP::request(HTTP::method::post, Port, Host, path, 
+    HTTP::request REST::POST (string path, map<string, string> params) const {
+        return HTTP::request (HTTP::method::post, URL {Port, Host, path},
             {{boost::beast::http::field::content_type, "application/x-www-form-urlencoded"}}, 
-              encode_form_data(params));
+              encode_form_data (params));
     }
 }
