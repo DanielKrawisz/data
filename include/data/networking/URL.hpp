99e9fea2a2a9cb88dcb91c9c2a375dafd0ea65d8
Katrina Knight <kat.knight@newgaea.net>
2023-01-17 21:37:27 +0000

    feat:Change Port to be string with optional number

--- include/data/networking/URL.hpp
+++ include/data/networking/URL.hpp
@@ -19,34 +19,28 @@ namespace data::networking {
         WSS
     };
 
-    uint16 default_port_number (protocol);
-
     struct port {
-        protocol Protocol;
-        optional<uint16> Number;
+        optional<string> Port;
 
-        uint16 number () const {
-            return bool(Number) ? *Number : default_port_number (Protocol);
-        }
-
-        port (const protocol p) : Protocol {p}, Number {} {}
-        port (const protocol p, uint16 number) : Protocol {p}, Number {number} {}
+        port (uint16 number) : Port {std::to_string(number)} {}
         port (const string &);
+
         operator string() const;
     };
+    port default_port_number (protocol);
 
     std::ostream &operator << (std::ostream &, const port &);
     std::istream &operator >> (std::istream &, port &);
     
     struct URL {
-        
+        protocol Protocol;
         port Port;
         string Host;
         string Path;
         list<entry<string, string>> Params;
         
-        URL (string port, string host, string path, list<entry<string, string>> params = {}) :
-            Port{port}, Host{host}, Path{path}, Params{params} {}
+        URL (protocol protocol,string port, string host, string path, list<entry<string, string>> params = {}) :
+            Protocol(protocol), Port{port}, Host{host}, Path{path}, Params{params} {}
         
         URL (const string &);
         
