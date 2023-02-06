4b2f4bc5f415b9f5d1a02a16ab5a18a040e8bdb1
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-02-06 17:32:22 -0500

    Generalize tcp_socket, which is now called just socket for all asio::socket types

--- src/data/io/run.cpp
+++ src/data/io/run.cpp
@@ -3,29 +3,38 @@
 
 #include <data/io/run.hpp>
 #include <data/net/async/wait_for_message.hpp>
+#include <data/net/asio/socket.hpp>
 
 namespace data::io {
 
     namespace bp = boost::process;
 
+    using pipe = bp::async_pipe;
+
+    using socket = net::asio::socket<bp::async_pipe>;
+
     // run an external command with standard in and standard out connected.
     void run (boost::asio::io_context &io, string command, error_handler err_handler, interaction i) {
 
-        ptr<bp::async_pipe> in { new bp::async_pipe {io}};
-        ptr<bp::async_pipe> out { new bp::async_pipe {io}};
-        ptr<bp::async_pipe> err { new bp::async_pipe {io}};
+        ptr<bp::async_pipe> in {new pipe {io}};
+        ptr<bp::async_pipe> out {new pipe {io}};
+        ptr<bp::async_pipe> err {new pipe {io}};
 
-        bp::child child {command, bp::std_out > *out, bp::std_err > *err, bp::std_in < *in};
+        ptr<bp::child> child { new bp::child (command, bp::std_out > *out, bp::std_err > *err, bp::std_in < *in) };
 
-        ptr<process> p {new process {std::move (child), net::async::message_queue<pipe, char> {in, err_handler}}};
+        ptr<socket> In { new socket {in}};
+        ptr<socket> Out { new socket {out}};
+        ptr<socket> Err { new socket {err}};
+
+        ptr<process> p {new process {std::move (child), In, err_handler}};
 
         auto handle = i (p);
 
-        net::async::wait_for_message<pipe, char> (out, [handle] (string_view x) -> void {
+        net::async::wait_for_message<string_view, error_code> (Out, [handle] (string_view x) -> void {
             handle->read_out (x);
         }, err_handler);
 
-        net::async::wait_for_message<pipe, char> (err, [handle] (string_view x) -> void {
+        net::async::wait_for_message<string_view, error_code> (Err, [handle] (string_view x) -> void {
             handle->read_err (x);
         }, err_handler);
 
