0c6e4d06ec0994907bc29455d4d2e0dba70b749b
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-02-17 20:25:05 -0500

    Add more curries

--- include/data/function.hpp
+++ include/data/function.hpp
@@ -26,6 +26,7 @@ namespace data {
         }
     };
 
+    // and the do nothing function.
     template <typename ... args> void do_nothing (args...) {}
     
     // Given an element of Y, we can construct a function from X to Y. 
@@ -56,10 +57,38 @@ namespace data {
         }
     };
 
-    template <typename n, typename x, typename... args>
-    function<n (args...)> inline curry (function<n (x, args...)> f, x var) {
-        return [f, var] (args... a) -> n {
-            return f (var, a...);
+    template <typename n, typename a, typename... args>
+    function<n (args...)> inline curry (function<n (a, args...)> f, a va) {
+        return [f, va] (args... z) -> n {
+            return f (va, z...);
+        };
+    }
+
+    template <typename n, typename a, typename b, typename... args>
+    function<n (args...)> inline curry (function<n (a, b, args...)> f, a va, b vb) {
+        return [f, va, vb] (args... z) -> n {
+            return f (va, vb, z...);
+        };
+    }
+
+    template <typename n, typename a, typename b, typename c, typename... args>
+    function<n (args...)> inline curry (function<n (a, b, c, args...)> f, a va, b vb, c vc) {
+        return [f, va, vb, vc] (args... z) -> n {
+            return f (va, vb, vc, z...);
+        };
+    }
+
+    template <typename n, typename a, typename b, typename c, typename d, typename... args>
+    function<n (args...)> inline curry (function<n (a, b, c, d, args...)> f, a va, b vb, c vc, d vd) {
+        return [f, va, vb, vc, vd] (args... z) -> n {
+            return f (va, vb, vc, vd, z...);
+        };
+    }
+
+    template <typename n, typename a, typename b, typename c, typename d, typename e, typename... args>
+    function<n (args...)> inline curry (function<n (a, b, c, d, e, args...)> f, a va, b vb, c vc, d vd, e ve) {
+        return [f, va, vb, vc, vd, ve] (args... z) -> n {
+            return f (va, vb, vc, vd, ve, z...);
         };
     }
 
