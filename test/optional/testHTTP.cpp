e87f78820052223fd6869999f71d493b29764d1d
KatrinaAS <kat@katcodes.dev>
2023-02-20 16:48:40 +0000

    feat: Add Optional Test framework

---
+++ test/optional/testHTTP.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 Daniel Krawisz
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "data/numbers.hpp"
+#include "gtest/gtest.h"
+
+namespace data {
+
+
+
+    TEST(HTTPTest, TestValid) {
+        
+        EXPECT_EQ(1,1);
+        
+    }
+}
+
