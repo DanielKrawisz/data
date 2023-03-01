bf475f3c0825cac737d77bb2317ad847e98ef1fc
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-03-01 12:17:52 -0500

    two kinds of serialized sessions, one for something like TCP and the other for something like websockets

--- include/data/net/serialized.hpp
+++ include/data/net/serialized.hpp
@@ -7,38 +7,44 @@
 #include <data/net/session.hpp>
 #include <data/parser.hpp>
 #include <data/function.hpp>
+#include <iostream>
 
 namespace data::net {
 
+    // an implementation of session that takes a message type that can be serialized into a string.
+    template <typename message_out, typename word = char>
+    struct serialized_session final : session<message_out> {
+
+        void send (message_out o) final override {
+            Out->send (Generator (o));
+        };
+
+        bool closed () final override {
+            return Out->closed ();
+        };
+
+        void close () final override {
+            return Out->close ();
+        };
+
+        serialized_session (function<std::basic_string<word> (const message_out &)> g) : Generator {g}, Out {} {}
+
+        // write as a string.
+        function<std::basic_string<word> (const message_out &)> Generator;
+
+        // the low-level session.
+        ptr<net::session<const std::basic_string<word> &>> Out;
+
+    };
+
+    // open a serialized session with a parser, meaning that the serialized message type
+    // can be read as a stream to determine where a message begins ends. We would use
+    // this with something like a TCP connection.
     template <typename message_out, typename message_in = message_out, typename word = char>
-    open<message_in, message_out> serialized_session (
+    open<message_in, message_out> serialized_session_parsed (
         open<std::basic_string_view<word>, const std::basic_string<word> &> Open,
         function<std::basic_string<word> (message_out)> Generator,
         function<ptr<writer<word>> (handler<const message_out &>)> Parser) {
-
-        struct out final : session<message_out> {
-
-            void send (message_out o) final override {
-                Out->send (Generator (o));
-            };
-
-            bool closed () final override {
-                return Out->closed ();
-            };
-
-            void close () final override {
-                return Out->close ();
-            };
-
-            out (function<std::basic_string<word> (const message_out &)> g) : Generator {g}, Out {} {}
-
-            // write as a string.
-            function<std::basic_string<word> (const message_out &)> Generator;
-
-            // the low-level session.
-            ptr<net::session<const std::basic_string<word> &>> Out;
-
-        };
 
         return [Open, Generator, Parser]
             (close_handler on_close, interaction<message_in, message_out> interact) -> void {
@@ -49,7 +55,8 @@ namespace data::net {
                     (ptr<session<const std::basic_string<word> &>> low_level) -> handler<std::basic_string_view<word>> {
 
                         // the high level out session. It does not know how to send messages yet.
-                        ptr<out> high_level = std::make_shared<out> (Generator);
+                        ptr<serialized_session<message_out, char>> high_level =
+                            std::make_shared<serialized_session<message_out, char>> (Generator);
 
                         // add the low level sesion to the high level session.
                         high_level->Out = low_level;
@@ -65,6 +72,44 @@ namespace data::net {
                     });
             };
     }
+
+    // open a serialized session with a reader, meaning that string that is received is known
+    // to be a complete message and we just read it into the deserialized type. We would use
+    // this with a websockets connection or something like that.
+    template <typename message_out, typename message_in = message_out, typename word = char>
+    open<message_in, message_out> serialized_session_read (
+        open<std::basic_string_view<word>, const std::basic_string<word> &> Open,
+        function<std::basic_string<word> (message_out)> Generator,
+        function<message_in (std::basic_string_view<word>)> Reader) {
+
+        return [Open, Generator, Reader]
+            (close_handler on_close, interaction<message_in, message_out> interact) -> void {
+
+                Open (on_close,
+                    // the receive handler.
+                    [interact, Generator, Reader]
+                    (ptr<session<const std::basic_string<word> &>> low_level) -> handler<std::basic_string_view<word>> {
+
+                        // the high level out session. It does not know how to send messages yet.
+                        ptr<serialized_session<message_out, char>> high_level =
+                            std::make_shared<serialized_session<message_out, char>> (Generator);
+
+                        // add the low level sesion to the high level session.
+                        high_level->Out = low_level;
+
+                        return [In = interact (std::static_pointer_cast<session<message_out>> (high_level)), Reader]
+                            (std::basic_string_view<word> x) {
+                                std::cout << "meep meep meep ... " << x << std::endl;
+                                auto j = Reader (x);
+                                std::cout << "moop moop moop" << std::endl;
+                                std::cout << "read message " << j << std::endl;
+                                In (j);
+                            };
+
+                    });
+            };
+    }
+
 
 }
 
