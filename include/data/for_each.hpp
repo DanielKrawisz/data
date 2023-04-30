23e204639f49cf4b99a28c449f2644b51de52d69
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-04-30 11:51:36 -0500

    improve error handling with websockets

--- include/data/for_each.hpp
+++ include/data/for_each.hpp
@@ -11,34 +11,34 @@
 namespace data {
     
     template <typename fun, typename input, 
-        typename element = std::remove_const_t<std::remove_reference_t<decltype(std::declval<input>().first())>>, 
-        typename output = std::remove_const_t<std::remove_reference_t<decltype(std::declval<fun>()(std::declval<element>()))>>>
+        typename element = std::remove_const_t<std::remove_reference_t<decltype (std::declval<input> ().first ())>>, 
+        typename output = std::remove_const_t<std::remove_reference_t<decltype (std::declval<fun> ()(std::declval<element> ()))>>>
     requires functional::function<fun, output, element> && sequence<input, element>
-    list<output> for_each(const fun& f, const input& i) {
-        return fold([&f](list<output> q, element x) -> list<output> {
-            return append(q, f(x));
-        }, list<output>{}, i);
+    list<output> for_each (const fun& f, const input& i) {
+        return fold ([&f] (list<output> q, element x) -> list<output> {
+            return append (q, f (x));
+        }, list<output> {}, i);
     }
     
     template <typename fun, typename input, 
-        typename key = std::remove_reference_t<decltype(std::declval<input>().values().first().key())>, 
-        typename value = std::remove_reference_t<decltype(std::declval<input>().values().first().value())>, 
-        typename output = std::remove_reference_t<decltype(std::declval<fun>()(std::declval<value>()))>>
+        typename key = std::remove_reference_t<decltype (std::declval<input> ().values ().first ().key ())>, 
+        typename value = std::remove_reference_t<decltype (std::declval<input> ().values ().first ().value ())>, 
+        typename output = std::remove_reference_t<decltype (std::declval<fun> () (std::declval<value> ()))>>
     requires functional::function<fun, output, value> && functional::map<input, key, value>
-    map<key, output> inline for_each(const fun& f, const input& i) {
-        return fold([&f](map<key, output> m, const entry<key, value>& e) -> map<key, output> {
-            return m.insert(e.Key, f(e.Value));
-        }, map<key, output>{}, i.values());
+    map<key, output> inline for_each (const fun& f, const input& i) {
+        return fold ([&f] (map<key, output> m, const entry<key, value>& e) -> map<key, output> {
+            return m.insert (e.Key, f (e.Value));
+        }, map<key, output> {}, i.values ());
     }
     
     template <typename fun, typename input, 
         typename element = std::remove_reference_t<decltype(std::declval<input>().values().first())>, 
         typename output = std::remove_reference_t<decltype(std::declval<fun>()(std::declval<element>()))>>
     requires functional::function<fun, output, element> && functional::ordered_set<input, element>
-    list<output> inline for_each(const fun& f, const input& i) {
-        return fold([&f](list<output> q, element x) -> list<output> {
-            return append(q, f(x));
-        }, list<output>{}, i.values());
+    list<output> inline for_each (const fun &f, const input &i) {
+        return fold ([&f] (list<output> q, element x) -> list<output> {
+            return append(q, f (x));
+        }, list<output> {}, i.values ());
     }
     /*
     template <typename fun, typename input, 
@@ -51,27 +51,29 @@ namespace data {
     }*/
     
     template <typename fun, typename element, 
-        typename output = std::remove_reference_t<decltype(std::declval<fun>()(std::declval<element>()))>>
+        typename output = std::remove_reference_t<decltype (std::declval<fun> () (std::declval<element> ()))>>
     requires functional::function<fun, output, element>
-    inline tree<output> for_each(const fun& f, const tree<element>& t) {
-        if (empty(t)) return {};
+    inline tree<output> for_each (const fun &f, const tree<element> &t) {
+        if (empty (t)) return {};
         
-        return {f(root(t)), for_each(f, left(t)), for_each(f, right(t))};
+        return {f (root (t)), for_each (f, left (t)), for_each (f, right (t))};
     }
     
     template <typename fun, typename element, 
-        typename output = std::remove_reference_t<decltype(std::declval<fun>()(std::declval<element>()))>>
+        typename output = std::remove_reference_t<decltype (std::declval<fun> () (std::declval<element> ()))>>
     requires functional::function<fun, output, element>
-    inline cross<output> for_each(const fun& f, const cross<element>& i) {
+    inline cross<output> for_each (const fun &f, const cross<element> &i) {
         cross<output> z;
-        z.resize(i.size());
-        auto a = i.begin();
-        auto b = z.begin();
-        while(a != i.end()) {
+        z.resize (i.size ());
+        auto a = i.begin ();
+        auto b = z.begin ();
+        
+        while (a != i.end ()) {
             *b = f(*a);
             a++;
             b++;
         }
+        
         return z;
     }
     
