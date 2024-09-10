1cb8f56779a77ed7a3e733fbefabe8b069f679b1
Daniel Krawisz <danielkrawisz@protonmail.com>
2024-09-10 12:46:51 -0500

    add function to get a yes/no from the user

---
+++ src/data/io/wait_for_enter.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Daniel Krawisz
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <limits>
+#include <data/io/wait_for_enter.hpp>
+
+namespace data {
+
+    void wait_for_enter (std::string message) {
+        std::cin.ignore (std::numeric_limits<std::streamsize>::max ());
+        do {
+            std::cout << '\n' << message;
+        } while (std::cin.get () != '\n');
+    }
+
+    bool get_user_yes_or_no (std::string message) {
+        std::cin.ignore (std::numeric_limits<std::streamsize>::max ());
+        char q;
+        do {
+            std::cout << message << " (Y/N)" << std::endl;
+            q = 0;
+            std::cin >> q;
+        } while (q != 'n' && q != 'y' && q != 'Y' && q != 'N');
+        return q == 'Y' || q == 'y';
+    }
+
+}
