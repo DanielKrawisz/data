2d6ce7506f58497dc5d8d3165b1faadf5f306a80
Daniel Krawisz <danielkrawisz@protonmail.com>
2024-09-18 11:42:20 -0500

    Add more tests for ordered list

--- test/testOrderedList.cpp
+++ test/testOrderedList.cpp
@@ -12,11 +12,22 @@ namespace data {
         ordered_list<int> a {1};
         ordered_list<int> b {1, 2};
         ordered_list<int> c {1, 2, 3};
+        ordered_list<int> ca {3, 2, 1};
+        ordered_list<int> cb {2, 3, 1};
+        ordered_list<int> cc {1, 3, 2};
+        ordered_list<int> cd {2, 1, 3};
+        ordered_list<int> ce {3, 1, 2};
         ordered_list<int> d;
 
         EXPECT_NE (a, d);
         EXPECT_NE (b, d);
         EXPECT_NE (c, d);
+
+        EXPECT_EQ (c, ca);
+        EXPECT_EQ (c, cb);
+        EXPECT_EQ (c, cc);
+        EXPECT_EQ (c, cd);
+        EXPECT_EQ (c, ce);
 
         // we had a problem with this earlier.
         d = c;
