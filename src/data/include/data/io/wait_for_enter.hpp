f38c7d0163843e74afe4f6d048710bb1a8d0a50f
Katrina Knight <kat.knight@newgaea.net>
2024-10-04 19:46:43 +0100

    fix: fix #129 made yes_no not break terminal

--- src/data/include/data/io/wait_for_enter.hpp
+++ src/data/include/data/io/wait_for_enter.hpp
@@ -6,12 +6,20 @@
 #define DATA_IO_WAIT_FOR_ENTER
 
 #include <iostream>
+#include <optional>
 
 namespace data {
-
+    /// @brief Waits for user to press enter, printing message again if anything other is pressed
+    /// @param message message to print
+    /// @warning Will cause an infinite loop if used in a non-terminal enviroment
     void wait_for_enter (std::string message = "Press enter to continue...");
 
-    bool get_user_yes_or_no (std::string message = "");
+
+    /// @brief Waits for user to respond to a message with yes or no
+    /// @param message question to ask user
+    /// @param default_value default value to return if enter pressed or no terminal
+    /// @return true if yes, false otherwise, or no value if nothing was chosen and no default
+    std::optional<bool> get_user_yes_or_no (std::string message ,std::string default_value="Y");
 
 }
 
--- src/data/io/wait_for_enter.cpp
+++ src/data/io/wait_for_enter.cpp
@@ -4,6 +4,8 @@
 
 #include <limits>
 #include <data/io/wait_for_enter.hpp>
+#include <unistd.h>
+#include <stdio.h>
 
 namespace data {
 
@@ -13,15 +15,19 @@ namespace data {
         } while (std::cin.get () != '\n');
     }
 
-    bool get_user_yes_or_no (std::string message) {
-        char q;
-        do {
+    std::optional<bool> get_user_yes_or_no (std::string message ,std::string default_value="Y") {
+            std::string input;
             std::cout << message << " (Y/N)" << std::endl;
-            q = 0;
-            std::cin >> q;
-        } while (q != 'n' && q != 'y' && q != 'Y' && q != 'N');
-        std::cin.ignore (std::numeric_limits<std::streamsize>::max (), '\n');
-        return q == 'Y' || q == 'y';
+            std::getline(std::cin,input);
+            if(input.length()>0){
+                char answer=std::tolower(input[0]);
+                if(answer=='y' || answer=='n')
+                    return answer=='y';
+                return {};
+            }
+            if(default_value!="")
+                return std::tolower(default_value[0])=='y';
+            return {};
     }
 
 }
