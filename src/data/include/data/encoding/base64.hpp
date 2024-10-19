64bc2c66b80c4140acd404eee4eadbcae8e34ad9
Katrina Knight <kat.knight@newgaea.net>
2024-10-19 17:11:55 +0100

    feat: move to cheaper regex, and added tests

--- src/data/include/data/encoding/base64.hpp
+++ src/data/include/data/encoding/base64.hpp
@@ -23,9 +23,11 @@ namespace data::encoding::base64 {
     }
     
     constexpr static char Pad = '=';
-    
-    constexpr static auto pattern = ctll::fixed_string {"(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})"};
 
+
+    //constexpr static auto pattern = ctll::fixed_string {"^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$"};
+    //
+    constexpr static auto pattern = ctll::fixed_string {"^(?=(.{4})*$)[A-Za-z0-9+/]*={0,2}$"};
     constexpr inline bool valid (string_view s) {
         return ctre::match<pattern> (s);
     }
