62bd438152eca0821b31d2dfd16313a29489455b
Daniel Krawisz <danielkrawisz@protonmail.com>
2024-10-22 10:41:28 -0500

    Update functional interfaces

--- test/testFunctionalInterfaces.cpp
+++ test/testFunctionalInterfaces.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "interface_tests.hpp"
+#include <data/tools.hpp>
 #include "gtest/gtest.h"
 
 // test whether data structures satisfy the correct interfaces. 
@@ -10,65 +10,53 @@ namespace data {
     
     TEST (FunctionalInterfaceTest, TestSequence) {
         
-        is_sequence<ordered_list<int>> ();
-        is_sequence<ordered_list<int *>> ();
-        is_sequence<ordered_list<int &>> ();
+        static_assert (sequence<ordered_list<int>>);
+        static_assert (sequence<ordered_list<int *>>);
+        static_assert (sequence<ordered_list<int &>>);
         
-        is_sequence<ordered_list<const int>> ();
-        is_sequence<ordered_list<const int *>> ();
-        is_sequence<ordered_list<const int &>> ();
+        static_assert (sequence<ordered_list<const int>>);
+        static_assert (sequence<ordered_list<const int *>>);
+        static_assert (sequence<ordered_list<const int &>>);
         
-        is_sequence<priority_queue<int>> ();
-        is_sequence<priority_queue<int *>> ();
-        is_sequence<priority_queue<int &>> ();
+        static_assert (sequence<priority_queue<int>>);
+        static_assert (sequence<priority_queue<int *>>);
+        static_assert (sequence<priority_queue<int &>>);
         
-        is_sequence<priority_queue<const int>> ();
-        is_sequence<priority_queue<const int *>> ();
-        is_sequence<priority_queue<const int &>> ();
+        static_assert (sequence<priority_queue<const int>>);
+        static_assert (sequence<priority_queue<const int *>>);
+        static_assert (sequence<priority_queue<const int &>>);
         
     }
     
     TEST (FunctionalInterfaceTest, TestContainer) {
         
-        is_container<ordered_list<int>, const int> ();
-        is_container<ordered_list<int *>, const int *> ();
-        is_container<ordered_list<int &>, const int &> ();
+        static_assert (container<ordered_list<int>, const int>);
+        static_assert (container<ordered_list<int *>, const int *>);
+        static_assert (container<ordered_list<int &>, const int>);
         
-        is_container<ordered_list<const int>, const int> ();
-        is_container<ordered_list<const int *>, const int *> ();
-        is_container<ordered_list<const int &>, const int> ();
+        static_assert (container<ordered_list<const int>, const int>);
+        static_assert (container<ordered_list<const int *>, const int *>);
+        static_assert (container<ordered_list<const int &>, const int>);
         
-        is_container<priority_queue<int>, const int> ();
-        is_container<priority_queue<int *>, const int *> ();
-        is_container<priority_queue<int &>, const int>();
+        static_assert (container<priority_queue<int>, const int>);
+        static_assert (container<priority_queue<int *>, const int *>);
+        static_assert (container<priority_queue<int &>, const int>);
         
-        is_container<priority_queue<const int>, const int> ();
-        is_container<priority_queue<const int *>, const int *> ();
-        is_container<priority_queue<const int &>, const int> ();
-        
-    }
-    
-    TEST (FunctionalInterfaceTest, TestList) {
-        
-        is_list<list<int>, int> ();
-        is_list<list<int *>, int *> ();
-        is_list<list<int &>, int &> ();
-        
-        is_list<list<const int>, const int> ();
-        is_list<list<const int *>, const int *> ();
-        is_list<list<const int &>, const int &> ();
+        static_assert (container<priority_queue<const int>, const int>);
+        static_assert (container<priority_queue<const int *>, const int *>);
+        static_assert (container<priority_queue<const int &>, const int>);
         
     }
-    
-    TEST (FunctionalInterfaceTest, TestTree) {
-        
-        is_tree<tree<int>> ();
-        is_tree<tree<int *>> ();
-        is_tree<tree<int &>> ();
-        
-        is_tree<tree<const int>> ();
-        is_tree<tree<const int *>> ();
-        is_tree<tree<const int &>> ();
-        
+
+    TEST (FunctionalInterfaceTest, TestIterable) {
+
+        static_assert (std::forward_iterator<decltype (std::declval<const ordered_list<int>> ().begin ())>);
+        static_assert (std::forward_iterator<decltype (std::declval<const ordered_list<int *>> ().begin ())>);
+        static_assert (std::forward_iterator<decltype (std::declval<const ordered_list<int &>> ().begin ())>);
+
+        static_assert (std::forward_iterator<decltype (std::declval<const ordered_list<const int>> ().begin ())>);
+        static_assert (std::forward_iterator<decltype (std::declval<const ordered_list<const int *>> ().begin ())>);
+        static_assert (std::forward_iterator<decltype (std::declval<const ordered_list<const int &>> ().begin ())>);
+
     }
 }
