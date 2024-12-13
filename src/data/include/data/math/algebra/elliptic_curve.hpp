9480ca79a7f202b5348444365ffb12a103b29ff9
Daniel Krawisz <danielkrawisz@protonmail.com>
2024-12-13 15:39:53 -0500

    Fill in some functions having to do with elliptic curves and geometry.

--- src/data/include/data/math/algebra/elliptic_curve.hpp
+++ src/data/include/data/math/algebra/elliptic_curve.hpp
@@ -9,6 +9,7 @@
 #include <data/math/number/modular.hpp>
 #include <data/math/field.hpp>
 #include <data/math/point.hpp>
+#include <data/math/power.hpp>
 
 namespace data::math {
 
@@ -17,12 +18,11 @@ namespace data::math {
         field A;
         field B;
 
-        field discriminant () const;
+        bool valid () const;
 
-        // make sure the curve is not singular.
-        bool valid () const {
-            return discriminant () != 0;
-        }
+        bool operator == (const Weierstrauss &) const;
+
+        field discriminant () const;
 
         struct point;
         struct compressed_point;
@@ -43,12 +43,20 @@ namespace data::math {
             const Weierstrauss &Curve;
             bool valid () const;
 
+            bool operator == (const projective_vector &) const;
+
             point operator * (const point &) const;
             point operator / (const point &) const;
 
             template <typename N> point operator ^ (const N &) const;
 
             compressed_point compress () const;
+
+            field x () const;
+            field y () const;
+
+        protected:
+            point (const Weierstrauss &curve, unsigned_limit<typename space::affine<field, 2>::point>);
         };
 
         struct compressed {
@@ -60,32 +68,194 @@ namespace data::math {
             const Weierstrauss &Curve;
             bool valid () const;
 
+            bool operator == (const projective_vector &) const;
+
             compressed_point operator * (const compressed_point &) const;
             compressed_point operator / (const compressed_point &) const;
 
-            template <typename N> point operator ^ (const N &) const;
+            template <typename N> compressed_point operator ^ (const N &) const;
 
             point uncompress () const;
+
+            field x () const;
+            field y () const;
         };
 
         struct projective_vector : space::projective<field, 3>::exterior<1> {
             const Weierstrauss &Curve;
             bool valid () const;
 
+            bool operator == (const projective_vector &) const;
+
             projective_vector operator * (const projective_vector &) const;
+            projective_vector operator / (const projective_vector &) const;
 
             template <typename N> projective_vector operator ^ (const N &) const;
+
+            field x () const;
+            field y () const;
+            field z () const;
         };
 
         struct Jacobian_point : space::projective<field, 3>::exterior<1> {
             const Weierstrauss &Curve;
             bool valid () const;
 
-            projective_vector operator * (const projective_vector &) const;
+            Jacobian_point operator * (const Jacobian_point &) const;
+            Jacobian_point operator / (const Jacobian_point &) const;
 
-            template <typename N> projective_vector operator ^ (const N &) const;
+            template <typename N> Jacobian_point operator ^ (const N &) const;
+
+            field x () const;
+            field y () const;
+            field z () const;
         };
     };
+
+    template <skew_field field>
+    bool inline Weierstrauss<field>::valid () const {
+        // make sure the curve is not singular.
+        return discriminant () != 0;
+    }
+
+    template <skew_field field>
+    field inline Weierstrauss<field>::discriminant () const {
+        return A * A * A * 4 + B * B * 27;
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::point inline Weierstrauss<field>::point::operator * (const point &x) const {
+        return Curve.multiply (*this, x);
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::point inline Weierstrauss<field>::point::operator / (const point &x) const {
+        return Curve.multiply (*this, Curve.invert (x));
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::compressed_point Weierstrauss<field>::compressed_point::operator * (const compressed_point &x) const {
+        return Curve.multiply (*this, x);
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::compressed_point Weierstrauss<field>::compressed_point::operator / (const compressed_point &x) const {
+        return Curve.multiply (*this, Curve.invert (x));
+    }
+
+    template <skew_field field>
+    bool inline Weierstrauss<field>::compressed_point::valid () const {
+        return uncompress ().valid ();
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::projective_vector inline Weierstrauss<field>::projective_vector::operator * (const projective_vector &x) const {
+        return Curve.multiply (*this, x);
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::projective_vector inline Weierstrauss<field>::projective_vector::operator / (const projective_vector &x) const {
+        return Curve.multiply (*this, Curve.invert (x));
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::Jacobian_point inline Weierstrauss<field>::Jacobian_point::operator * (const Jacobian_point &x) const {
+        return Curve.multiply (*this, x);
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::Jacobian_point inline Weierstrauss<field>::Jacobian_point::operator / (const Jacobian_point &x) const {
+        return Curve.multiply (*this, Curve.invert (x));
+    }
+
+    template <skew_field field> template <typename N>
+    Weierstrauss<field>::compressed_point inline Weierstrauss<field>::compressed_point::operator ^ (const N &n) const {
+        return (this->uncompress () ^ n).compress ();
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::compressed_point inline Weierstrauss<field>::invert (const compressed_point &u) {
+        return invert (u.uncompress ()).compress ();
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::compressed_point inline Weierstrauss<field>::multiply (const compressed_point &a, const compressed_point &b) {
+        return multiply (a.uncompress (), b.uncompress ()).compress ();
+    }
+
+    template <skew_field field> template <typename N>
+    Weierstrauss<field>::point inline Weierstrauss<field>::point::operator ^ (const N &n) const {
+        return data::pow<field, N> (*this, n);
+    }
+
+    template <skew_field field> template <typename N>
+    Weierstrauss<field>::projective_vector inline Weierstrauss<field>::projective_vector::operator ^ (const N &n) const {
+        return data::pow<field, N> (*this, n);
+    }
+
+    template <skew_field field> template <typename N>
+    Weierstrauss<field>::Jacobian_point inline Weierstrauss<field>::Jacobian_point::operator ^ (const N &n) const {
+        return data::pow<field, N> (*this, n);
+    }
+
+    template <skew_field field>
+    bool inline Weierstrauss<field>::point::valid () const {
+        return is_infinite (*this) || y () * y () == x () * x () * x () + A * x () + B;
+    }
+
+    template <skew_field field>
+    bool inline Weierstrauss<field>::projective_vector::valid () const {
+        return y () * y () * z () == x () * x () * x () + A * x () * z () * z () + B * z () * z () * z ();
+    }
+
+    template <skew_field field>
+    bool Weierstrauss<field>::Jacobian_point::valid () const {
+        auto zz = z () * z ();
+        auto zzz = zz * z ();
+        auto zzzz = zz * zz;
+        auto zzzzzz = zzz * zzz;
+        return y () * y () == x () * x () * x () + A * x () * zzzz + B * zzzzzz;
+    }
+/*
+    template <skew_field field>
+    Weierstrauss<field>::point multiply (const point &a, const point &b) {
+        if (is_infinite (a)) return b;
+        if (is_infinite (b)) return a;
+        if (a == b) return
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::projective_vector Weierstrauss<field>::multiply (const projective_vector &a, const projective_vector &b) {
+
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::Jacobian_point Weierstrauss<field>::multiply (const Jacobian_point &a, const Jacobian_point &b) {
+
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::point invert (const point &p) {
+        if (is_infinite (p)) return p;
+        return point {p.x (), field {1} / p.y ()};
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::compressed_point invert (const compressed_point &p) {
+        if (is_infinite (p)) return p;
+        compressed z = p.Value.get<compressed> ();
+        return compressed {z.X, -z.YSign};
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::projective_vector invert (const projective_vector &p) {
+        return projective_vector {x (), field {1} / y (), z ()};
+    }
+
+    template <skew_field field>
+    Weierstrauss<field>::Jacobian_point invert (const Jacobian_point &p) {
+        return Jacobian_point {x (), field {1} / y (), z ()};
+    }*/
     
 }
 
--- src/data/include/data/math/point.hpp
+++ src/data/include/data/math/point.hpp
@@ -9,50 +9,74 @@
 #include <data/math/linear/space.hpp>
 #include <data/math/algebra/algebra.hpp>
 
-namespace data::math {
+namespace data::math::linear {
 
     template <skew_field X, size_t A, size_t B> using matrix = array<X, A, B>;
 
     template <skew_field X, size_t A, size_t B> X det (const matrix<X, A, B> &);
     template <skew_field X, size_t A> X det (const matrix<X, A, A> &);
 
+    template <skew_field X, size_t A, size_t B> bool invertable (const matrix<X, A, B> &);
+    template <skew_field X, size_t A> bool invertable (const matrix<X, A, A> &);
+    template <skew_field X, size_t A> matrix<X, A, A> inverse (const matrix<X, A, A> &);
+
 };
 
 namespace data::math::space {
 
+    // the exterior algebra comprises scalars, vectors, asymmetric 2-tensors, etc.
     template <skew_field X, size_t order, size_t dim> struct exterior;
 
     template <skew_field X, size_t dim> using scalar = exterior<X, 0, dim>;
     template <skew_field X, size_t dim> using vector = exterior<X, 1, dim>;
-
-    template <skew_field X, size_t A, size_t B, size_t dim>
-    exterior <X, A + B, dim> operator ^ (const exterior<X, A, dim> &, const exterior<X, B, dim> &);
-
-    template <skew_field X, size_t order, size_t dim>
-    exterior <X, dim - order, dim> operator * (const exterior<X, order, dim> &);
-
-    template <skew_field X, size_t order, size_t dim>
-    X operator * (const exterior<X, order, dim> &, const exterior<X, dim - order, dim> &);
-
-    template <ordered Q, skew_field X, size_t order, size_t dim> requires requires (const X &x, const X &y) {
-        {x * inner (x, y)} -> implicitly_convertible_to<Q>;
-    } Q operator * (const exterior<X, order, dim> &a, const exterior<X, order, dim> &b);
+    // nonstandard terminology but it helps to remember what these things are.
+    template <skew_field X, size_t dim> using planar = exterior<X, 2, dim>;
+    template <skew_field X, size_t dim> using spacer = exterior<X, 3, dim>;
 
     // scalar multiplication
     template <skew_field X, size_t u, size_t dim>
     exterior <X, u, dim> operator * (const exterior<X, u, dim> &, const X &);
 
+    // addition
     template <skew_field X, size_t u, size_t dim>
     exterior <X, u, dim> operator + (const exterior<X, u, dim> &, const exterior<X, u, dim> &);
 
+    template <skew_field X, size_t A, size_t B, size_t dim>
+    exterior <X, A + B, dim> operator ^ (const exterior<X, A, dim> &, const exterior<X, B, dim> &);
+
+    template <skew_field X, size_t order, size_t dim>
+    X operator * (const exterior<X, order, dim> &, const exterior<X, dim - order, dim> &);
+
+    // generate a matrix that projects onto the subspace defined by the exterior object.
+    // the second exterior object is left unchanged.
+    template <skew_field X, size_t order, size_t dim>
+    linear::matrix<X, dim, dim> projector (const exterior<X, order, dim> &a, const exterior<X, dim - order, dim> &b);
+
+    template <skew_field X, size_t order, size_t dim> requires requires (const X &x, const X &y) {
+        {x * inner (x, y)};
+    } auto operator * (const exterior<X, order, dim> &a, const exterior<X, order, dim> &b);
+
+    // Hodge star
+    template <skew_field X, size_t order, size_t dim>
+    requires requires (const X &x, const X &y) {
+        {x * inner (x, y)};
+    } exterior<X, dim - order, dim> operator * (const exterior<X, order, dim> &);
+
     // affine geometry is like Euclid without circles.
+    // technically, we do not have the full exterior algebra
+    // available because we don't have the Hodge star without
+    // a notion of perpendicularity.
     template <skew_field X, size_t dim> struct affine {
+        // technically, this is an incorrect use of the term.
         template <size_t order> struct simplex;
 
         using point = simplex<0>;
         using line = simplex<1>;
         using plane = simplex<2>;
         using space = simplex<3>;
+
+        using vector = typename space::vector<X, dim>;
+        using planar = typename space::planar<X, dim>;
 
         // whether a line contains a point, etc.
         template <size_t a, size_t b>
@@ -63,18 +87,21 @@ namespace data::math::space {
         static simplex<a + b> connect (const simplex<a> &, const simplex<b> &);
 
         // we represent objects as exterior algebraic objects of one higher dimension.
-        template <size_t order> struct simplex : exterior<X, order + 1, dim + 1> {
-            // valid if a valid exterior object and nonzero.
+        template <size_t order> struct simplex : space::exterior<X, order + 1, dim + 1> {
+            simplex (std::initializer_list<simplex<order - 1>>);
+
+            // valid if nonzero and not parallel to the affine subspace.
             bool valid () const;
 
-            simplex operator + (const exterior<X, order, dim> &v) const;
+            // subtract a simplex from a simplex and you get an exterior object.
+            // (ie, point - point = vector.
+            exterior<X, order + 1, dim> operator - (const simplex &p) const;
 
-            exterior<X, order, dim> operator - (const simplex &p) const;
+            // and of course we can also invert the former operation.
+            simplex operator + (const exterior<X, order, dim> &v) const;
         };
 
-        struct transformation : matrix<X, dim + 1, dim + 1> {
-            transformation (const simplex<dim> &from, const simplex<dim> &to);
-
+        struct transformation : linear::matrix<X, dim + 1, dim + 1> {
             bool valid () const;
 
             transformation operator * (const transformation &) const;
@@ -82,6 +109,13 @@ namespace data::math::space {
             template <size_t order>
             simplex<order> operator * (const simplex<order> &) const;
         };
+
+        static transformation translation (const vector &);
+        static transformation scale (const X &);
+
+        // flip over the first exterior object, leave the other unchanged (must be linearly independent).
+        template <size_t order>
+        static transformation flip (const exterior<X, order, dim> &, const exterior<X, dim - order, dim> &);
     };
 
     template <skew_field X, ordered Q, size_t dim> requires normed<X, Q>
@@ -96,7 +130,11 @@ namespace data::math::space {
         // distance squared
         Q quadrance (const point &, const point &);
 
+        // rational trigonometry
+        Q spread (const line &, const line &);
+
         struct transformation : affine<X, dim>::transformation {
+            // must be orthogonal.
             bool valid () const;
 
             transformation operator * (const transformation &) const;
@@ -104,16 +142,22 @@ namespace data::math::space {
 
         static transformation translation (const vector<X, dim> &);
         static transformation scale (const X &);
-        static transformation flip (const vector<X, dim> &);
-        // from two flips we can get a rotation. We do not have a native
-        // function for rotations because you cannot do an arbitrary
+
+        // flip over the exterior object, leave everything perpendicular to it unchanged.
+        // rotations can be made out of two vector flips multiplied together. We do not
+        // have a native function for rotations because you cannot do an arbitrary
         // rotation, depending on the field.
+        template <size_t order>
+        static transformation flip (const exterior<X, order, dim> &);
     };
 
-    template <skew_field X, ordered Q, size_t dim> requires normed<X, Q>
-    struct elliptic {
+    template <skew_field X, ordered Q, size_t dim>
+    struct elliptic : Euclidian<X, Q, dim> {
         template <size_t order>
-        struct simplex : exterior<X, order + 1, dim + 1> {
+        struct simplex : Euclidian<X, Q, dim>::simplex<order> {
+            using Euclidian<X, Q, dim>::template simplex<order>::simplex;
+
+            // must be of quadrance 1.
             bool valid () const;
         };
 
@@ -125,9 +169,11 @@ namespace data::math::space {
         template <size_t a, size_t b>
         static bool contains (const simplex<a> &, const simplex<b> &);
 
-        struct transformation : simplex<2> {
-            // must be orthogonal.
+        struct transformation : linear::matrix<X, dim + 1, dim + 1> {
+            // must be orthogonal
             bool valid () const;
+
+            transformation operator * (const transformation &) const;
 
             template <size_t order>
             simplex<order> operator * (const simplex<order> &) const;
@@ -135,21 +181,22 @@ namespace data::math::space {
     };
 
     template <skew_field X, size_t dim> struct projective {
+        template <size_t order> using affine_exterior = space::template exterior<X, order, dim + 1>;
+
         template <size_t order>
-        struct exterior : space::exterior<X, order, dim> {
+        struct exterior : affine_exterior<order + 1> {
+            using affine_exterior<order + 1>::affine_exterior;
             bool valid () const;
 
+            // two exterior objects in projective space are equal if they are proportional to one another.
             bool operator == (const exterior &x) const;
         };
 
         using value = unsigned_limit<X>;
 
         template <size_t order>
-        struct simplex : space::exterior<value, order + 1, dim + 1> {
-
+        struct simplex : space::exterior<value, order, dim> {
             bool valid () const;
-
-            bool operator == (const simplex &x) const;
         };
 
         using point = simplex<1>;
@@ -160,8 +207,10 @@ namespace data::math::space {
         struct transformation : exterior<2> {
             transformation (const simplex<dim> &from, const simplex<dim> &to);
             bool valid () const;
+
             template <size_t order>
             transformation operator * (const exterior<order> &) const;
+
             template <size_t order>
             simplex<order> operator * (const simplex<order> &) const;
         };
@@ -181,12 +230,89 @@ namespace data::math::space {
 
     template <skew_field X, size_t order, size_t dim> struct exterior : box_array<X, order, dim>::type {
         using array = box_array<X, order, dim>::type;
+        using array::array;
         exterior (array &&);
         exterior (const array &);
         // it's valid if the symmetry properties are correct.
         bool valid ();
     };
     
+}
+
+namespace data::math::linear {
+
+    template <skew_field X, size_t A, size_t B> X inline det (const matrix<X, A, B> &) {
+        return 0;
+    }
+
+    template <skew_field X, size_t A, size_t B> bool inline invertable (const matrix<X, A, B> &) {
+        return false;
+    }
+
+    template <skew_field X, size_t A> bool inline invertable (const matrix<X, A, A> &m) {
+        return det (m) != 0;
+    }
+
+}
+
+// NOTE: hereafter we use X {0} to represent the zero element of the field and X {1} to represent the unit.
+// this is not necessarilly correct for all fields but it ought to work in ordinary cases. To be perfect we
+// may need to be more careful in a later version.
+
+namespace data::math::space {
+    // valid if nonzero.
+    template <skew_field X, size_t dim> template <size_t order>
+    bool inline projective<X, dim>::exterior<order>::valid () const {
+        return affine_exterior<order + 1>::valid () && static_cast<affine_exterior<order + 1>> (*this) != affine_exterior<order + 1> {0};
+    }
+
+    template <skew_field X, size_t dim>
+    template <size_t order>
+    bool inline projective<X, dim>::exterior<order>::operator == (const exterior &x) const {
+        auto ia = this->begin ();
+        auto ib = x.begin ();
+        // the ratio between elements in these exterior objects.
+        X ratio {0};
+        while (ia != this->end ()) {
+            // if any element is zero when the corresponding element in the
+            // other object is nonzere, then these are definitely not equal.
+            if (*ia == X {0} && *ib != X {0} || *ia != X {0} && *ib == X {0}) return false;
+            if (*ia == X {0} && *ib == X {0}) {
+                if (ratio == X {0}) ratio = *ia / *ib;
+                else if (ratio != *ia / *ib) return false;
+            }
+        }
+
+        return true;
+    }
+
+    template <skew_field X, size_t dim> template <size_t order>
+    bool projective<X, dim>::simplex<order>::valid () const {
+        if (!space::template exterior<value, order, dim>::valid ()) return false;
+        // infinite values are not allowed to appear after the first finite value.
+        bool first_finite_value = false;
+        for (const value &v : *this)
+            if (!is_infinite (v)) first_finite_value = true;
+            else if (first_finite_value) return false;
+        return true;
+    };
+
+    template <skew_field X, ordered Q, size_t dim> template <size_t order>
+    bool inline elliptic<X, Q, dim>::simplex<order>::valid () const {
+        return static_cast<exterior<X, order + 1, dim + 1>> (*this) * static_cast<exterior<X, order + 1, dim + 1>> (*this) == X {0};
+    }
+
+    template <skew_field X, size_t dim>
+    bool affine<X, dim>::transformation::valid () const {
+        if (!invertable (*this) && (*this)[dim * dim - 1] != X {1}) return false;
+        for (int index = dim * (dim - 1); index < dim * dim - 1; index++) if ((*this)[index] != X {0}) return false;
+        return true;
+    }
+
+    template <skew_field X, size_t dim> template <size_t order>
+    affine<X, dim>::transformation inline affine<X, dim>::flip (const exterior<X, order, dim> &a, const exterior<X, dim - order, dim> &b) {
+        return projector (b, a) - projector (a, b);
+    }
 }
 
 #endif
