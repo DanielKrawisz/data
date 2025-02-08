51958813f467d63acb3a74d60217e98cf40b9dbe
Daniel Krawisz <danielkrawisz@protonmail.com>
2025-02-08 11:23:16 -0500

    Add function to get double from user

--- src/data/io/wait_for_enter.cpp
+++ src/data/io/wait_for_enter.cpp
@@ -16,52 +16,74 @@ namespace data {
         } while (std::cin.get () != '\n');
     }
 
-   bool get_user_yes_or_no (std::string message ,maybe<bool> default_value) {
-            std::string input;
-            while(true) {
-                std::cout << message << " (";
-                std::cout << ((default_value.has_value() && default_value.value()) ? "Y":"y");
-                std::cout << "/";
-                std::cout << ((default_value.has_value() && !default_value.value()) ? "N":"n");
-                std::cout << ")" << std::endl;
-                std::getline(std::cin,input);
+    bool get_user_yes_or_no (std::string message, maybe<bool> default_value) {
+                std::string input;
+                while(true) {
+                    std::cout << message << " (";
+                    std::cout << ((default_value.has_value () && default_value.value()) ? "Y":"y");
+                    std::cout << "/";
+                    std::cout << ((default_value.has_value () && !default_value.value()) ? "N":"n");
+                    std::cout << ")" << std::endl;
+                    std::getline (std::cin,input);
 
-                if(input.length()>0){
-                    char answer=std::tolower(input[0]);
-                    if(answer=='y' || answer=='n')
-                        return answer=='y';
-                    continue;
+                    if (input.length () > 0){
+                        char answer = std::tolower (input[0]);
+                        if (answer =='y' || answer =='n')
+                            return answer=='y';
+                        continue;
+                    }
+
+                    if (default_value.has_value ())
+                        return default_value.value ();
                 }
-                if(default_value.has_value())
-                    return default_value.value();
-            }
+        }
+
+    std::string get_user_password (std::string message, char mask) {
+        struct termios term, original;
+        std::string password;
+
+        // Get current terminal settings
+        if (tcgetattr (STDIN_FILENO, &term) != 0)
+            return "";
+
+        original = term;
+        // Unset ECHO flag
+        term.c_lflag &= ~ECHO & ~ICANON;
+        if (tcsetattr (STDIN_FILENO, TCSANOW, &term) != 0)
+            return "";
+        // Prompt for password
+        std::cout << message;
+
+        char c;
+
+        while (true) {
+            c = getchar ();
+            if (c!='\n') break;
+            std::cout << mask;
+            password.push_back (c);
+        }
+
+        std::cout << std::endl;
+        // Set ECHO flag
+        tcsetattr (STDIN_FILENO, TCSANOW, &original);
+        return password;
     }
 
-std::string get_user_password(std::string message, char mask)
-{
-    struct termios term, original;
-    std::string password;
-     // Get current terminal settings
-    if (tcgetattr(STDIN_FILENO, &term) != 0)
-        return "";
-    original = term;
-    // Unset ECHO flag
-    term.c_lflag &= ~ECHO & ~ICANON;
-    if (tcsetattr(STDIN_FILENO, TCSANOW, &term) != 0)
-        return "";
-    // Prompt for password
-    std::cout << message;
-    
-    char c;
-    c=getchar();
-    while(c!='\n') {
-        std::cout << mask;
-        password.push_back(c);
-        c=getchar();
+    double read_decimal (const std::string &question) {
+        double value;
+        while (true) {
+            std::cout << question << " ";
+            std::cin >> value;
+
+            // Check if input was valid
+            if (std::cin.fail ()) {
+                std::cin.clear ();  // Clear error flag
+                std::cin.ignore (std::numeric_limits<std::streamsize>::max (), '\n'); // Discard invalid input
+                std::cout << "Invalid input. Please enter a decimal number.\n";
+            } else {
+                std::cin.ignore (std::numeric_limits<std::streamsize>::max (), '\n'); // Clear buffer
+                return value;
+            }
+        }
     }
-    std::cout<<std::endl;
-     // Set ECHO flag
-    tcsetattr(STDIN_FILENO, TCSANOW, &original);
-    return password;
-}
 }
