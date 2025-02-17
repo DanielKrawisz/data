b92f0330ce977cf2639fddd0218c7d3e49edd619
Katrina Knight <kat@katcodes.dev>
2025-02-17 16:45:38 +0000

    fix: fix Find SECP256K1

--- cmake/FindSECP256K1.cmake
+++ cmake/FindSECP256K1.cmake
@@ -26,6 +26,14 @@ if( NOT SECP256K1_in_cache )
             HINTS ENV SECP256K1_INC_DIR
                   ENV SECP256K1_DIR
                   $ENV{SECP256K1_DIR}/include
+            PATHS
+              /usr/include
+              /usr/local/include
+              /opt/local/include
+              $ENV{ProgramFiles}/libsecp256k1/include
+              $ENV{ProgramFiles\(x86\)}/libsecp256k1/include
+              "C:/Program Files/libsecp256k1/include"
+              "C:/Program Files (x86)/libsecp256k1/include"
             PATH_SUFFIXES include
   	        DOC "The directory containing the SECP256K1 header files"
            )
@@ -34,12 +42,38 @@ if( NOT SECP256K1_in_cache )
     HINTS ENV SECP256K1_LIB_DIR
           ENV SECP256K1_DIR
           $ENV{SECP256K1_DIR}/lib
+    PATHS
+            /usr/lib
+            /usr/local/lib
+            /opt/local/lib
+            $ENV{ProgramFiles}/libsecp256k1/lib
+            $ENV{ProgramFiles\(x86\)}/libsecp256k1/lib
+            "C:/Program Files/libsecp256k1/lib"
+            "C:/Program Files (x86)/libsecp256k1/lib"
     PATH_SUFFIXES lib
     DOC "Path to the Release SECP256K1 library"
     )
 
 set(SECP256K1_LIBRARIES ${SECP256K1_LIBRARY_RELEASE})
 
+if(WIN32)
+        find_file(SECP256K1_DLL
+            NAMES libsecp256k1-5.dll
+            HINTS
+                ENV SECP256K1_BIN_DIR
+                ENV SECP256K1_DIR
+                ${SECP256K1_DIR}/bin
+            PATHS
+                $ENV{ProgramFiles}/libsecp256k1/bin
+                $ENV{ProgramFiles\(x86\)}/libsecp256k1/bin
+                "C:/Program Files/libsecp256k1/bin"
+                "C:/Program Files (x86)/libsecp256k1/bin"
+            PATH_SUFFIXES bin
+            DOC "Path to the SECP256K1 DLL (Windows only)"
+        )
+    endif()
+
+    set(SECP256K1_DLLS ${SECP256K1_DLL})
 
   # Attempt to load a user-defined configuration for SECP256K1 if couldn't be found
   if ( NOT SECP256K1_INCLUDE_DIR OR NOT SECP256K1_LIBRARIES)
@@ -49,3 +83,4 @@ set(SECP256K1_LIBRARIES ${SECP256K1_LIBRARY_RELEASE})
 endif()
 
 find_package_handle_standard_args(SECP256K1 "DEFAULT_MSG" SECP256K1_LIBRARIES SECP256K1_INCLUDE_DIR)
+mark_as_advanced(SECP256K1_INCLUDE_DIR SECP256K1_LIBRARIES SECP256K1_DLLS)
