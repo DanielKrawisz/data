0db0166482e612aba1fc128c46587975f5b32764
Daniel Krawisz <danielkrawisz@protonmail.com>
2025-04-29 07:31:35 -0500

    Fixed another wrong file path

--- test/testHash.cpp
+++ test/testHash.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "data/crypto/hash/hash.hpp"
+#include "data/crypto/hash.hpp"
 #include "data/math/number/bytes.hpp"
 #include "data/arithmetic/endian.hpp"
 #include "gtest/gtest.h"
