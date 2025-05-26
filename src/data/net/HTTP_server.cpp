3b1efa8a8b7ee998ef671b45ece45510d4ecfffc
Daniel Krawisz <danielkrawisz@protonmail.com>
2025-05-26 15:09:51 -0500

    The HTTP server now keeps track of all sessions and cancels them on shutdown.

--- src/data/net/HTTP_server.cpp
+++ src/data/net/HTTP_server.cpp
@@ -18,3 +18,91 @@ namespace data::net::HTTP::beast {
     }
 
 }
+
+namespace data::net::HTTP {
+    server::server (exec ex, net::IP::TCP::endpoint ep, request_handler handler):
+        Acceptor {ex}, Handler {std::move (handler)} {
+
+        asio::error ec;
+        asio::ip::tcp::endpoint endpoint (ep);
+
+        // Open the acceptor
+        Acceptor.open (endpoint.protocol (), ec);
+        if (ec) throw std::runtime_error ("Error opening acceptor: " + ec.message ());
+
+        // Bind to the endpoint
+        Acceptor.bind (endpoint, ec);
+        if (ec) throw std::runtime_error ("Error binding acceptor: " + ec.message ());
+
+        // Start listening for connections
+        Acceptor.listen (asio::socket_base::max_listen_connections, ec);
+        if (ec) throw std::runtime_error ("Error listening for connections: " + ec.message ());
+    }
+
+    awaitable<bool> server::accept () {
+        if (!Acceptor.is_open ()) co_return false;
+
+        try {
+            asio::ip::tcp::socket socket = co_await Acceptor.async_accept (asio::use_awaitable);
+            auto ex = co_await asio::this_coro::executor;
+            // Spawn a new session to handle the connection
+            spawn (ex,
+                [sock = std::move (socket), self = this] () mutable -> awaitable<void> {
+                    auto xxx = std::make_shared<session> (self->Sessions, std::move (sock), self->Handler);
+                    self->Sessions.add (xxx);
+                    co_await xxx->read ();
+                    co_return;
+                });
+
+            // yield if single-threaded
+            co_await asio::post (ex, asio::use_awaitable);
+        } catch (const boost::system::system_error &e) {
+            if (e.code () == boost::asio::error::operation_aborted) {
+                co_return false;  // The acceptor was closed—time to shut down
+            } else throw e;
+        }
+
+        co_return true;
+    }
+
+    awaitable<void> server::session::read () {
+        while (true) {
+            beast::flat_buffer buff;
+            beast::request req;
+            try {
+                co_await beast::http::async_read (Socket, buff, req, asio::use_awaitable);
+            } catch (const boost::system::system_error &e) {
+                if (e.code() == boost::asio::error::eof) {
+                    // Remote side closed connection
+                    shutdown ();
+                    co_return;
+                } else if (e.code () == boost::asio::error::operation_aborted || e.code () == boost::asio::error::bad_descriptor) {
+                    // You cancelled this (e.g. during shutdown)
+                    co_return;
+                } else throw e;
+            }
+
+            co_await handle_request (req);
+
+            if (!req.keep_alive ()) break;
+        }
+    }
+
+    void server::sessions::remove_all () {
+        std::lock_guard<std::mutex> lock (Mtx);
+        auto x = Sessions.begin ();
+        while (x != Sessions.end ()) {
+            x = Sessions.erase (x);
+        }
+    }
+
+    void server::sessions::add (ptr<session>x) {
+        std::lock_guard<std::mutex> lock (Mtx);
+        Sessions.insert (x);
+    }
+
+    void server::sessions::remove (ptr<session> x) {
+        std::lock_guard<std::mutex> lock (Mtx);
+        Sessions.erase (x);
+    }
+}
