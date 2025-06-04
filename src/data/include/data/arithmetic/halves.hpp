019f981ba32b9bb030fbe86103908024983be2a7
Daniel Krawisz <danielkrawisz@protonmail.com>
2025-06-03 20:08:19 -0500

    Add special case of half_of for 128 bit types.

--- src/data/include/data/arithmetic/halves.hpp
+++ src/data/include/data/arithmetic/halves.hpp
@@ -10,28 +10,29 @@
 #include <data/encoding/endian.hpp>
 
 namespace data::encoding {
-    template<std::size_t S> struct int_by_size;
-    template<> struct int_by_size<1> { using type = std::int8_t; };
-    template<> struct int_by_size<2> { using type = std::int16_t; };
-    template<> struct int_by_size<4> { using type = std::int32_t; };
-    template<> struct int_by_size<8> { using type = std::int64_t; };
+    template <std::size_t S> struct int_by_size;
+    template <> struct int_by_size<1> { using type = std::int8_t; };
+    template <> struct int_by_size<2> { using type = std::int16_t; };
+    template <> struct int_by_size<4> { using type = std::int32_t; };
+    template <> struct int_by_size<8> { using type = std::int64_t; };
 
-    template<std::size_t S> struct uint_by_size;
-    template<> struct uint_by_size<1> { using type = std::uint8_t; };
-    template<> struct uint_by_size<2> { using type = std::uint16_t; };
-    template<> struct uint_by_size<4> { using type = std::uint32_t; };
-    template<> struct uint_by_size<8> { using type = std::uint64_t; };
+    template <std::size_t S> struct uint_by_size;
+    template <> struct uint_by_size<1> { using type = std::uint8_t; };
+    template <> struct uint_by_size<2> { using type = std::uint16_t; };
+    template <> struct uint_by_size<4> { using type = std::uint32_t; };
+    template <> struct uint_by_size<8> { using type = std::uint64_t; };
 
 #if defined(__SIZEOF_INT128__)
-    template<> struct uint_by_size<16> { using type = __uint128_t; };
-    template<> struct int_by_size<16> { using type = __int128_t; };
+    template <> struct uint_by_size<16> { using type = __uint128_t; };
+    template <> struct int_by_size<16> { using type = __int128_t; };
 #endif
 }
 
 namespace data {
+    template <typename T> struct half_of;
 
-    template<std::unsigned_integral T>
-    struct half_of : encoding::uint_by_size<sizeof (T) / 2> {
+    template <std::unsigned_integral T>
+    struct half_of<T> : encoding::uint_by_size<sizeof (T) / 2> {
         using type = encoding::uint_by_size<sizeof (T) / 2>::type;
 
         static type greater_half (T u) {
@@ -43,12 +44,59 @@ namespace data {
         }
     };
 
+    template <std::signed_integral T>
+    struct half_of<T> : encoding::int_by_size<sizeof (T) / 2> {
+        using type = encoding::int_by_size<sizeof (T) / 2>::type;
+
+        static type greater_half (T u) {
+            return u >> sizeof (type) * 8;
+        }
+
+        static type lesser_half (T u) {
+            return static_cast<T> (u & std::numeric_limits<type>::max ());
+        }
+    };
+
+#if defined(__SIZEOF_INT128__)
+
+    template <>
+    struct half_of<uint128> : encoding::uint_by_size<8> {
+        using type = encoding::uint_by_size<8>::type;
+
+        static type greater_half (uint128 u) {
+            return u >> sizeof (type) * 8;
+        }
+
+        static type lesser_half (uint128 u) {
+            return static_cast<uint128> (u & std::numeric_limits<type>::max ());
+        }
+    };
+
+    template <>
+    struct half_of<int128> : encoding::int_by_size<8> {
+        using type = encoding::int_by_size<8>::type;
+
+        static type greater_half (int128 u) {
+            return u >> sizeof (type) * 8;
+        }
+
+        static type lesser_half (int128 u) {
+            return static_cast<int128> (u & std::numeric_limits<type>::max ());
+        }
+    };
+#endif
+
     template<typename T, typename = void>
     struct twice { }; // leave undefined or empty when invalid
 
-    template<typename T>
+    template<std::unsigned_integral T>
     struct twice<T, std::void_t<typename encoding::uint_by_size<sizeof(T) * 2>::type>> {
         using type = typename encoding::uint_by_size<sizeof(T) * 2>::type;
+    };
+
+    template<std::signed_integral T>
+    struct twice<T, std::void_t<typename encoding::int_by_size<sizeof(T) * 2>::type>> {
+        using type = typename encoding::int_by_size<sizeof(T) * 2>::type;
     };
 
     // Aliases for ease of use
