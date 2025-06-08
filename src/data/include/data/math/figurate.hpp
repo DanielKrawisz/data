dd67778f4d2aa077f1ade0fa79f76d66e5d26975
Daniel Krawisz <danielkrawisz@protonmail.com>
2025-06-08 18:12:07 -0500

    Add some stuff to geometry.

--- src/data/include/data/math/figurate.hpp
+++ src/data/include/data/math/figurate.hpp
@@ -13,7 +13,7 @@ namespace data::math {
         negative_factorial () : std::invalid_argument ("factorial of zero is undefined") {}
     };
 
-    template <typename N> nonzero<N> factorial (const N &n) {
+    template <typename N> constexpr nonzero<N> factorial (const N &n) {
         if (n < 0) throw negative_factorial {};
         if (n < 2) return nonzero<N> {1};
         N z = n;
@@ -22,7 +22,7 @@ namespace data::math {
         return nonzero<N> {z};
     }
 
-    template <typename N> N binomial (const N &n, const N &k) {
+    template <typename N> constexpr N binomial (const N &n, const N &k) {
         if (n < 0 || k < 0 || k > n) throw negative_factorial {};
         if (n < 2 || k == 0) return 1;
         N m = n - k;
@@ -32,24 +32,24 @@ namespace data::math {
         return divide<N, N> {} (z, factorial<N> (k)).Quotient;
     }
 
-    template <typename N> N inline multichoose (const N &n, const N &r) {
+    template <typename N> constexpr N inline multichoose (const N &n, const N &r) {
         return binomial (n + r - 1, r);
     }
 
-    template <typename N> N inline polytopic_number (const N &r, const N &n) {
+    template <typename N> constexpr N inline polytopic_number (const N &r, const N &n) {
         if (n == 0) return 0;
         return multichoose (n, r);
     }
 
-    template <typename N> N inline triangular_number (const N &n) {
+    template <typename N> constexpr N inline triangular_number (const N &n) {
         return polytopic_number<N> (2, n);
     }
 
-    template <typename N> N inline tetrahedral_number (const N &n) {
+    template <typename N> constexpr N inline tetrahedral_number (const N &n) {
         return polytopic_number<N> (3, n);
     }
 
-    template <typename N> N inline pentatope_number (const N &n) {
+    template <typename N> constexpr N inline pentatope_number (const N &n) {
         return polytopic_number<N> (4, n);
     }
 }
--- test/testGeometry.cpp
+++ test/testGeometry.cpp
@@ -3,9 +3,33 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "gtest/gtest.h"
+#include <data/math.hpp>
+#include <data/math/linear/inner.hpp>
 #include <data/math/point.hpp>
+#include <data/math/algebra/finite_field.hpp>
 
-namespace data::crypto {
+namespace data::math {
+
+    static_assert (linear::space<Q, space::exterior<Q, 1, 0>>);
+    static_assert (linear::space<Q, space::exterior<Q, 1, 1>>);
+    static_assert (linear::space<Q, space::exterior<Q, 2, 0>>);
+    static_assert (linear::space<Q, space::exterior<Q, 2, 1>>);
+    static_assert (linear::space<Q, space::exterior<Q, 2, 2>>);
+    static_assert (linear::space<Q, space::exterior<Q, 3, 0>>);
+    static_assert (linear::space<Q, space::exterior<Q, 3, 1>>);
+    static_assert (linear::space<Q, space::exterior<Q, 3, 2>>);
+    static_assert (linear::space<Q, space::exterior<Q, 3, 3>>);
+
+
+    TEST (GeometryTest, TestScalarConstructor) {
+        space::exterior<Q, 1, 0> {2};
+        space::exterior<Q, 2, 0> {2};
+        space::exterior<Q, 3, 0> {2};
+        space::exterior<Q, 4, 0> {2};
+        space::exterior<Q, 5, 0> {2};
+        space::exterior<Q, 6, 0> {2};
+        space::exterior<Q, 7, 0> {2};
+    }
 
     // test euclidian transformations.
 
