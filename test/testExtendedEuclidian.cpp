669dc7a23b013b7e0305adf3b786119e88b884c1
Daniel Krawisz <danielkrawisz@protonmail.com>
2025-06-28 10:50:08 -0700

    Extended Euclidian test now uses a typed test suite

--- test/testExtendedEuclidian.cpp
+++ test/testExtendedEuclidian.cpp
@@ -16,10 +16,48 @@ namespace data::math {
     using Zb2 = Z_bytes_BC_big;
     using Zl2 = Z_bytes_BC_little;
 
-    template <typename N, typename Z>
-    void extended_euclidian_test () {
+    template <typename tuple>
+    struct extended_euclidian_test : ::testing::Test {
+        using natural = typename std::tuple_element<1, tuple>::type;
+        using integer = typename std::tuple_element<0, tuple>::type;
+    };
+
+    using test_cases = ::testing::Types<
+        tuple<uint64, uint64>, 
+        tuple<int64, int64>, 
+        tuple<uint64_little, uint64_little>,
+        tuple<int64_little, int64_little>,
+        tuple<uint64_big, uint64_big>,
+        tuple<int64_big, int64_big>,
+        tuple<uint80, uint80>,
+        tuple<uint80_little, uint80_little>,
+        tuple<uint80_big, uint80_big>,
+        tuple<int80, int80>,
+        tuple<int80_little, int80_little>,
+        tuple<int80_big, int80_big>,
+        tuple<int128, int128>,
+        tuple<int128_little, int128_little>,
+        tuple<int128_big, int128_big>,
+        tuple<uint128, uint128>,
+        tuple<uint128_little, uint128_little>,
+        tuple<uint128_big, uint128_big>,
+        tuple<Z, N>,
+        tuple<Zl1, Nl>,
+        tuple<Zb1, Nb>,
+        tuple<Z_bytes_BC_little, Z_bytes_BC_little>,
+        tuple<Z_bytes_BC_big, Z_bytes_BC_big>,
+        tuple<dec_int, dec_uint>,
+        tuple<hex_int, hex_uint>,
+        tuple<hex_int_BC, hex_int_BC>>;
+
+    TYPED_TEST_SUITE (extended_euclidian_test, test_cases);
+
+    TYPED_TEST (extended_euclidian_test, TestType) {
+        using N = typename TestFixture::natural;
+        using Z = typename TestFixture::integer;
+
         using X = math::number::euclidian::extended<N, Z>;
-        EXPECT_THROW (X::algorithm (N {0}, N {0}).GCD, math::division_by_zero);
+        EXPECT_THROW ((void) X::algorithm (N {0}, N {0}).GCD, math::division_by_zero);
 
         EXPECT_EQ (X::algorithm (N {1}, N {1}).GCD, 1);
 
@@ -27,55 +65,19 @@ namespace data::math {
 
         EXPECT_EQ (X::algorithm (N {1145}, N {916}).GCD, 229);
     }
-    
+
     template <typename Z, typename N>
     struct test_extended_euclidian {
         test_extended_euclidian () {
-            extended_euclidian_test<N, Z> ();
-            extended_euclidian_test<Z, Z> ();
+            using X = math::number::euclidian::extended<N, Z>;
+            EXPECT_THROW (X::algorithm (N {0}, N {0}).GCD, math::division_by_zero);
+
+            EXPECT_EQ (X::algorithm (N {1}, N {1}).GCD, 1);
+
+            EXPECT_EQ (X::algorithm (N {2}, N {4}).GCD, 2);
+
+            EXPECT_EQ (X::algorithm (N {1145}, N {916}).GCD, 229);
         }
     };
-    
-    TEST (ExtendedEuclidianTest, TestExtendedEuclidian) {
-
-        test_extended_euclidian<uint64, uint64> {};
-        test_extended_euclidian<int64, int64> {};
-
-        test_extended_euclidian<uint64_little, uint64_little> {};
-        test_extended_euclidian<int64_little, int64_little> {};
-
-        test_extended_euclidian<uint64_big, uint64_big> {};
-        test_extended_euclidian<int64_big, int64_big> {};
-
-        test_extended_euclidian<int80, int80> {};
-        test_extended_euclidian<int80_little, int80_little> {};
-        test_extended_euclidian<int80_big, int80_big> {};
-
-        test_extended_euclidian<int128, int128> {};
-        test_extended_euclidian<int128_little, int128_little> {};
-        test_extended_euclidian<int128_big, int128_big> {};
-
-        test_extended_euclidian<uint80, uint80> {};
-        test_extended_euclidian<uint80_little, uint80_little> {};
-        test_extended_euclidian<uint80_big, uint80_big> {};
-
-        test_extended_euclidian<uint128, uint128> {};
-        test_extended_euclidian<uint128_little, uint128_little> {};
-        test_extended_euclidian<uint128_big, uint128_big> {};
-
-        test_extended_euclidian<Z, N> {};
-
-        test_extended_euclidian<Zl1, Nl> {};
-        test_extended_euclidian<Zb1, Nb> {};
-
-        test_extended_euclidian<Z_bytes_BC_little, Z_bytes_BC_little> {};
-        test_extended_euclidian<Z_bytes_BC_big, Z_bytes_BC_big> {};
-
-        test_extended_euclidian<dec_int, dec_uint> {};
-        test_extended_euclidian<hex_int, hex_uint> {};
-
-        test_extended_euclidian<hex_int_BC, hex_int_BC> {};
-        
-    }
     
 }
