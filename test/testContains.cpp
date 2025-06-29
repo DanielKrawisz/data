da005a1b9548d99ca04c35fac23cf33e737a3b5c
Daniel Krawisz <danielkrawisz@protonmail.com>
2025-06-29 09:41:16 -0700

    Update dependencies. Improve tests for contains to ensure that all functional data structures can use the contains function. Use boost::endian::endian_load and endian_store for sv.

--- test/testContains.cpp
+++ test/testContains.cpp
@@ -9,6 +9,13 @@ namespace data {
     template <typename elem> using vector = std::vector<elem>;
 
     TEST (ContainsTest, TestContains) {
+        using stack_has_contains = decltype (contains (std::declval<stack<string>> (), "xyz"));
+        using list_has_contains = decltype (contains (std::declval<list<string>> (), "xyz"));
+        using tree_has_contains = decltype (contains (std::declval<tree<string>> (), "xyz"));
+        using set_has_contains = decltype (contains (std::declval<tree<string>> (), "xyz"));
+        using ordered_list_has_contains = decltype (contains (std::declval<ordered_list<string>> (), "xyz"));
+        using priority_queue_has_contains = decltype (contains (std::declval<priority_queue<string>> (), "xyz"));
+        using map_has_contains = decltype (contains (std::declval<map<string, string>> (), "xyz"));
 
         EXPECT_TRUE (contains (stack<string> {"a", "b", "c", "d"}, "a"));
         EXPECT_FALSE (contains (stack<string> {"a", "b", "c", "d"}, "f"));
